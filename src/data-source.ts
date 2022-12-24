feede384a296755b3be90a2a7e0810e324264e9b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-24 23:08:21 +0200

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-//import { User } from "./entity/User"
+import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "database.sqlite",
     synchronize: true,
     logging: false,
-    entities: ["src/entity/**/*.ts", "test/entity-mocks/**/*.ts"],
-    migrations: ["src/migration/**/*.ts"],
-    subscribers: ["src/subscriber/**/*.ts"]
+    entities: [User],
+    migrations: [],
+    subscribers: []
 })
 
 AppDataSource.initialize()
