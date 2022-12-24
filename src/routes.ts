7e2ddbf8e29c112efe9f68c725ad1980b19cedcb
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-24 15:49:37 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,21 @@
-console.log("routes")
-export {}
+var express = require('./app');
+
+var apiv1 = express.Router();
+
+apiv1.get('/', function(req, res) {
+   res.json({"Articles":[
+    {
+        title:"d",
+        id:1,
+        url:"d/d/d"
+    },
+    {
+        title:"d",
+        id:2,
+        url:"d/d/d"
+    }
+  ]});
+
+});
+
+module.exports = apiv1;
