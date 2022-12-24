224d4a8c9ccd990a197167cd859dcba3d857e00b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2022-12-24 13:12:45 +0200

    init

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+
+import "reflect-metadata";
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req:any, res:any) => {
+  res.json({"Articles":[
+    {
+        title:"d",
+        id:1,
+        url:"d/d/d"
+    },
+    {
+        title:"d",
+        id:2,
+        url:"d/d/d"
+    }
+  ]})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
