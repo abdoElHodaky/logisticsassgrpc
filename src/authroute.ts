0096f126e6368a4e9d1902ba669cf6d25a03ea2b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-12-26 20:12:07 +0200

    Update authroute.ts

--- src/authroute.ts
+++ src/authroute.ts
@@ -6,9 +6,10 @@ export const authroute=Router();
 authroute.post("/auth/signup/",(req,res)=>{
     let user:User=<User>{...req.body}
     console.log(req.body)
-    AppDataSource.manager.save(User,user).then(d=>{
+    res.json(user)
+    /*AppDataSource.manager.save(User,user).then(d=>{
         res.json({messsage:"created succefully",user:user})
-    }).catch(console.log)
+    }).catch(console.log)*/
 })
 
 authroute.post("/auth/login/",(req,res)=>{
