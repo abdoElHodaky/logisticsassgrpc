d3ecd7e661bed953aa6b3be15a8c753f4ccf6b06
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2023-01-03 16:16:26 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,10 @@ import "reflect-metadata";
 import application from "express"
 import { json,urlencoded } from "express";
 import cors from "cors";
+import { AppDataSource } from "./_datasource";
 import { apiv1 } from "./routes";
 const app=application();
 const port = process.env.PORT||3000
-
-/*app.get('/', (req:any, res:any) => {
-  res.json({"Articles":[
-    {
-        title:"d",
-        id:1,
-        url:"d/d/d"
-    },
-    {
-        title:"d",
-        id:2,
-        url:"d/d/d"
-    }
-  ]})
-})*/
 
 
 app.use(urlencoded({extended: true}))
