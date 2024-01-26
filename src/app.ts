1a5bcf78e9297e803992c5946bef5fc435ab016c
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-01-26 10:36:07 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,8 +32,11 @@ app.use((error:any, req:any, res:any, next:any) => {
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-app.use('/endpoints', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.get("/",(req,res)=>{
+    res.redirect("endpoints")
+})
+app.use('/endpoints', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
