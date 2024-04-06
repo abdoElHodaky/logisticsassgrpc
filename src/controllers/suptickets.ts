d19f4f7bc9f9a34902e3127353f4ec6c2ff76320
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 18:22:32 +0200

    Update suptickets.ts

--- src/controllers/suptickets.ts
+++ src/controllers/suptickets.ts
@@ -1,1 +1,28 @@
 
+import { supTicket } from "../entity/supTicket"
+import { User } from "../entity/User"
+import { AppDataSource } from "../_datasource";
+//import { CreateArticleDto } from "../dto/create-article.dto"
+import { Res, Post, Controller, Get, Body } from '@decorators/express';
+import { Response ,Request} from "express"
+
+
+@Controller('/articles')
+export class ArticleController {
+
+  constructor() {}
+
+  @Get("/")
+  async all(@Res() res:Response) {
+   /* 	#swagger.tags = ['Article']
+        #swagger.description = 'Endpoint to get articles' */
+
+    
+    
+  }
+
+  @Post("/create")
+  //async create(@Res() res:Response ,@Body() createArticleDto:CreateArticleDto){
+   //  #swagger.tags = ['Article']
+    
+}
