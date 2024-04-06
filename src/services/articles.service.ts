a06b02b3f10ed99b3d5079a852a7874afc4f39f9
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 12:44:26 +0200

    Update articles.service.ts

--- src/services/articles.service.ts
+++ src/services/articles.service.ts
@@ -2,7 +2,7 @@ import { Author } from "../entity/Author";
 import { Article } from "../entity/Article"
 import { Author } from "../entity/Author"
 import { AppDataSource } from "../_datasource";
-import { CreateArticle } from "../dto/create-article.dto"
+import { CreateArticleDto } from "../dto/create-article.dto"
 
 export class ArticleService {
   constructor (){}
