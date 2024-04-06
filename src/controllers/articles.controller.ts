ab1eaee0a297f6e4812f193c020e5fd0a7241e1f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 12:15:21 +0200

    Update articles.controller.ts

--- src/controllers/articles.controller.ts
+++ src/controllers/articles.controller.ts
@@ -1,5 +1,6 @@
 import { Author } from "../entity/Author";
 import { Article } from "../entity/Article"
+import { Author } from "../entity/Author"
 import { AppDataSource } from "../_datasource";
 
 import {
@@ -13,7 +14,32 @@ class ArticleController {
   constructor() {}
 
   @Get('articles')
-  All(@Response() res) {
-    res.send(this.userService.findById(id));
+  async all(@Response() res) {
+    let articles=await AppDataSource.manager.find(Article)
+    res.json(articles)
+  }
+
+  @Post("create")
+  async create(@Body() createArticleDto:CreateArticleDto){
+    let {article,useris}=createArticleDto
+    let article=<Article>{...article}
+    let author:Author=await AppDataSource.manager.findOneByOrFail(Author,{id:userid})
+    article.author=author
+    author.articles.push(article)
+    await article.save()
+    res.json({message:"created successfully"})
+    /*AppDataSource.manager.findOneByOrFail(Author,{id:userid}).then(d=>{
+        author=d;
+        return author
+    }).then(a=>{
+        article.author=a;
+        a.articles=[]
+        a.articles.push(article)
+        return article
+    }).then(a=>{
+        AppDataSource.manager.save(Article,a)
+        //AppDataSource.manager.save(a.user)
+        res.json({message:"created successfully"})
+    })*/
   }
 }
