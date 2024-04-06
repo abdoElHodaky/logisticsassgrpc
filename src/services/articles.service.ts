8d857ead2ff8e6c444bb4ddb8468fcab3ff11ef3
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 15:27:38 +0200

    Update articles.service.ts

--- src/services/articles.service.ts
+++ src/services/articles.service.ts
@@ -1,8 +1,10 @@
+import { Injectable } from "@decorators/di";
 import { Article } from "../entity/Article"
 import { Author } from "../entity/Author"
 import { AppDataSource } from "../_datasource";
 import { CreateArticleDto } from "../dto/create-article.dto"
 
+@Injectable()
 export class ArticleService {
   constructor (){}
 
