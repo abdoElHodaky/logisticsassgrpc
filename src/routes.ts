7313a1c9f4eccba4ce1d6437b912187221acb16b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 19:28:54 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./controllers/auths";
 import { supTicket } from "./entity/supTicket";
 import { User } from "./entity/User";
 import { UserController } from "./controllers/users";
+import { userTicketController } from "./controllers/users.tickets";
 import { AppDataSource } from "./_datasource";
 import { ArticleController } from "./controllers/articles";
 import { AuthorController } from "./controllers/authors";
@@ -43,7 +44,7 @@ apiv1.post("/suptickets/create",(req,res)=>{
 attachControllers(apiv1,[AuthController])
 //apiv1.use(authroute)
 //apiv1.use(usersroute);
-attachControllers(apiv1,[UserController])
+attachControllers(apiv1,[UserController,userTicketController])
 attacbControllers(apiv1,[AuthorController])
 //apiv1.use(authorsroutes)
 attachControllers(apiv1,[ArticleController])
