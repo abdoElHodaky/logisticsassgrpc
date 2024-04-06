54d3660fc5186421780eafe636f522c31086d07f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 12:03:47 +0200

    Update articles.controller.ts

--- src/controllers/articles.controller.ts
+++ src/controllers/articles.controller.ts
@@ -1,1 +1,15 @@
+import {
+  Response, Params, Controller, Get,
+  attachControllers, Middleware
+} from '@decorators/express';
 
+@Controller('/')
+class ArticleController {
+
+  constructor() {}
+
+  @Get('/articles')
+  getData(@Response() res) {
+    res.send(this.userService.findById(id));
+  }
+}
