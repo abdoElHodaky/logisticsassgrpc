774ef257beb75ad15628e66f4be0a4d7cdefa384
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 19:12:19 +0200

    Update users.tickets.ts

--- src/controllers/users.tickets.ts
+++ src/controllers/users.tickets.ts
@@ -1,1 +1,9 @@
+import { User } from "../entity/User"
+import { AppDataSource } from "../_datasource";
+//import { CreateArticleDto } from "../dto/create-article.dto"
+import { Res, Post, Controller, Get, Body } from '@decorators/express';
+import { Response ,Request} from "express"
+import { supTicketController } from "./suptickets";
 
+@Controller('/users/:user/tickets')
+export class userTicketController extends supTicketController {}
