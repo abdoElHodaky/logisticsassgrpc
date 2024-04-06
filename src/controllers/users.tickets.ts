eda99863319aba40412e42ca8a226b6e128581ac
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 19:26:33 +0200

    Update users.tickets.ts

--- src/controllers/users.tickets.ts
+++ src/controllers/users.tickets.ts
@@ -3,7 +3,23 @@ import { AppDataSource } from "../_datasource";
 //import { CreateArticleDto } from "../dto/create-article.dto"
 import { Res, Post, Controller, Get, Body } from '@decorators/express';
 import { Response ,Request} from "express"
-import { supTicketController } from "./suptickets";
+import { supTicket } from "../entity/supTicket";
 
-@Controller('/users/:user/tickets')
-export class userTicketController extends supTicketController {}
+@Controller('/users/:userix/tickets/')
+export class userTicketController extends supTicketController {
+
+  @Get("")
+  async all(@Params("userid") userid:string, @Res() res:Response):Promise<supTicket|void>
+  {
+    let id=Number(userid)
+    let user=await AppDataSource.manager.findOneOrFail(User,{where:{
+            id:id
+           },
+           relations:{
+            tickets:true
+           }
+            })
+    let tickets=user.tickets
+    res.json(tickets)
+  }
+}
