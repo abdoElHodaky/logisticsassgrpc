b51bdaf0584e1c1b57c51193dc1ac89a7154779a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 15:59:46 +0200

    Update users.ts

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,8 +2,9 @@ import { Article } from "../entity/Article"
 import { User } from "../entity/User"
 import { AppDataSource } from "../_datasource";
 //import { CreateArticleDto } from "../dto/create-article.dto"
-import { Res, Post, Controller, Get, Body } from '@decorators/express';
+import { Res, Post, Controller, Get, Body , Params } from '@decorators/express';
 import { Response ,Request} from "express"
+import { isNumeric,nationalIdvalid } from "../helpers";
 
 
 @Controller('/users')
@@ -15,4 +16,31 @@ export class UserController {
     let resd:User[]=await AppDataSource.getRepository(User).find()
     return resd
   }
+
+  @Get("/:id")
+  async user(@Params("id") id:string, @Res() res: Response ):Promise<User> |void 
+  {
+    /* 	#swagger.tags = ['User']
+        #swagger.description = 'Endpoint to sign in a specific user' */
+
+    if(isNumeric(id)==true){
+      console.log(nationalIdvalid(id))
+      id=Number(id)
+     let user=await AppDataSource.getRepository(User).findOneOrFail({
+        where:{
+          id:id
+        },
+        relations:{
+          tickets:true,
+          verifications:true
+        }
+      })
+      return user
+    }
+    else{
+       res.json({message:"user not found or you used invalid paramter"})
+    }
+  }
+
+  
 }
