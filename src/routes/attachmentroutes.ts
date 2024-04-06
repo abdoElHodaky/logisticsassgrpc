3ffc88d389371c572778b6a03793a162f960263d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 22:56:32 +0200

    Update attachmentroutes.ts

--- src/routes/attachmentroutes.ts
+++ src/routes/attachmentroutes.ts
@@ -6,6 +6,12 @@ import { booksroute } from "./bookroutes";
 export const attachmentsroute=Router()
 
 attachmentsroute.get("/attachments/",(req,res)=>{
+    /* 	#swagger.tags = ['Attachment']
+        #swagger.description = 'Endpoint to get attachments' 
+        
+    
+    */
+    
     AppDataSource.manager.find(Attachment).
     then(d=>{
         res.json(d)
