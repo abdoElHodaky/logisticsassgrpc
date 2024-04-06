adca0b7d7d307c4682d2d77613c455dea7c2e144
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-06 14:10:13 +0200

    Update swagautogen.js

--- swagautogen.js
+++ swagautogen.js
@@ -1,8 +1,7 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./dist/routes/*.js','./dist/routes.js'];
-
+const endpointsFiles = ['./dist/routes/*.js','./dist/routes.js','./dist/controllers/*.controller.js'];
 const config = {
     info: {
         title: 'Elearning endpoints docs',
@@ -10,7 +9,7 @@ const config = {
     },
     tags: [ ],
     host: '',
-    schemes: [],
+    schemes: ["./dist/dto/*"],
 };
 
 swaggerAutogen(outputFile, endpointsFiles, config);
