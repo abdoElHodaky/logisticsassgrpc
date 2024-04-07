43684c1c53dcef34394cbde0ccdfc75846158a3b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 15:14:14 +0200

    Update auth.ts

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 import { Injectable , Inject } from "@decorators/di";
 import { User } from "../entity/"
 import { DataSource ,AppDataSource  } from "../includes"
-import { CreateUserDto } from "../dto/create-user.dto"
+import { LoginUserDto } from "../dto/login-user.dto"
 import { isNumeric,nationalIdvalid } from "../helpers";
 
 //@Injectable()
@@ -10,8 +10,15 @@ export class AuthService {
   public  datasource:DataSource=AppDataSource
   constructor ( ){}
 
-  async login():Promise<User>{
-    
+  async login(loginUserDto:LoginUserDto):Promise<User>{
+    const { username,passwordHash}=loginUserDto
+     let user= await this.datasource.manager.findOneOrFail(User,{where:{
+        username:username,
+        passwordHash:passwordHash,
+       // id:id
+    }})
+    return user
+
   }
   
   async create():Promise<User|void> {
