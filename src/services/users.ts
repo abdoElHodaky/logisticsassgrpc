9e01c61d2e3ca344301841065d4a87b3ec396d8b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 11:25:58 +0200

    Update users.ts

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,7 +1,7 @@
 import { Injectable , Inject } from "@decorators/di";
 import { DataSource } from "typeorm"
 import { Article,User,Author } from "../entity/"
-import { AppDataSource , _datasourceInject } from "../data-source";
+import {  _datasourceInject } from "../data-source";
 import { CreateArticleDto } from "../dto/create-article.dto"
 import { isNumeric,nationalIdvalid } from "../helpers";
 //@Injectable()
