22ca9b604926b5b21fce0f4f1b319ed8bc296ff9
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 10:25:54 +0200

    Update articles.ts

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -9,7 +9,7 @@ import { CreateArticleDto } from "../dto/create-article.dto"
 export class ArticleService {
   constructor (@Inject(_datasourceInject) private readonly datasource: DataSource){}
 
-  async all():Promise<Article>{
+  async all():Promise<Article[]>{
     return await this.datasource.manager.find(Article)
   }
 }
