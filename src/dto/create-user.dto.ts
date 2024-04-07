5aab1563bcfd47f4a226733a5ce2dc7111a22682
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 18:53:57 +0200

    Update create-user.dto.ts

--- src/dto/create-user.dto.ts
+++ src/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-
+export class CreateUserDto{
+  readonly username:string;
+  readonly firstname:string;
+  readonly lastname:string;
+  readonly IDcardNumber: number;
+  readonly email:string;
+  readonly age:number;
+  readonly type:string;
+}
