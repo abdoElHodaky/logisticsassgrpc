598e5cfe65a67be2765dc4dd7b19f512f842ffee
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 09:51:09 +0200

    Update articles.service.ts

--- src/services/articles.service.ts
+++ src/services/articles.service.ts
@@ -2,12 +2,12 @@ import { Injectable , Inject } from "@decorators/di";
 import { DataSource } from "typeorm"
 import { Article } from "../entity/Article"
 import { Author } from "../entity/Author"
-import { AppDataSource ,datasourceinject } from "../_datasource";
+import { AppDataSource , _datasourceInject } from "../_datasource";
 import { CreateArticleDto } from "../dto/create-article.dto"
 
 @Injectable()
 export class ArticleService {
-  constructor (@Inject(datasourceinject) datasource: DataSource){}
+  constructor (@Inject(_datasourceInject) datasource: DataSource){}
 
   async all():Promise<any>{
     return
