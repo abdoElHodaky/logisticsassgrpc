7ed303d34046ced18ad2d5b7b9ac058d1bf14023
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 15:56:12 +0200

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,4 +2,4 @@ import {AuthService} from "./auth";
 import {UserService} from "./users";
 import {AuthorService} from "./authors";
 import {ArticleService} from "./articles";
-import {UserTicketService} from "./auth";
+import {UserTicketService} from "./users.tickets";
