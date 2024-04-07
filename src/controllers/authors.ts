d9252e22a8cf4137063c423c911e756604f9972b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 13:13:02 +0200

    Update authors.ts

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -1,3 +1,5 @@
+import { Inject } from "@decorators/di";
+import { AuthorService} from "../services/authors";
 import { Article } from "../entity/Article"
 import { Author } from "../entity/Author"
 import { AppDataSource } from "../_datasource";
@@ -9,7 +11,7 @@ import { isNumeric,nationalIdvalid } from "../helpers";
 
 @Controller('/authors')
 export class AuthorController {
-  constructor(){}
+  constructor(@Inject("authorS") private readonly authorS:AuthorService ){}
   
   @Get("/")
   async all():Promise<Author[]>{
