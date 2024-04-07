622856e3c3e4aeba0fcec61d5b6f0c2a487061cf
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 10:08:01 +0200

    Update articles.ts

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -2,7 +2,7 @@ import { Injectable , Inject } from "@decorators/di";
 import { DataSource } from "typeorm"
 import { Article } from "../entity/Article"
 import { Author } from "../entity/Author"
-import { AppDataSource , _datasourceInject } from "../_datasource";
+import { AppDataSource , _datasourceInject } from "../data-source";
 import { CreateArticleDto } from "../dto/create-article.dto"
 
 @Injectable()
