dda2daf35ec634bac81e1f5fd19954e382a933bb
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 10:02:12 +0200

    Update and rename articles.service.ts to articles.ts

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -7,9 +7,9 @@ import { CreateArticleDto } from "../dto/create-article.dto"
 
 @Injectable()
 export class ArticleService {
-  constructor (@Inject(_datasourceInject) datasource: DataSource){}
+  constructor (@Inject(_datasourceInject) private readonly datasource: DataSource){}
 
-  async all():Promise<any>{
-    return 
+  async all():Promise<Article>{
+    return await this.datasource.manager.find(Article)
   }
 }
