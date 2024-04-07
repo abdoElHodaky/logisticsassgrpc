dda96b5240b7470498acb3b88600780c98a479af
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 16:45:04 +0200

    Update articles.ts

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -13,4 +13,16 @@ export class ArticleService {
     //console.log(this._source)
     return await this.datasource.manager.find(Article)
   }
+
+ async create(createArticleDto:CreateArticleDto):Promise<Article:void>{
+     
+     let {article,userid}=createArticleDto
+    article=<Article>{...article}
+    let author=await this.datasource.manager.findOneByOrFail(Author,{id:parseInt(userid)})
+    article.author=author
+    author.articles.push(article)
+    article=await this.datasource.manager.save(Article,article)
+    return article
+   
+ }
 }
