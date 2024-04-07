65f2a22370f3b422b0bea4c1719d65f12267656e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 16:35:25 +0200

    Update attachments.ts

--- src/controllers/attachments.ts
+++ src/controllers/attachments.ts
@@ -1,1 +1,22 @@
+import { AttachmentService} from "../services/";
+import { Attachment } from "../entity/"
+//import { AppDataSource } from "../_datasource";
+//import { CreateArticleDto } from "../dto/create-article.dto"
+import { Res, Post, Controller, Get, Body } from '@decorators/express';
+import { Response ,Request} from "express"
 
+
+@Controller('/attachments/')
+export class AttachmentController {
+  
+  private attachmentS:AttachmentService=new AttachmentService()
+  
+  constructor( ) {}
+
+  @Get("")
+  async all(@Res() res:Response) {
+   /* 	#swagger.tags = ['Article']
+        #swagger.description = 'Endpoint to get articles' */
+    let attachments=await this.attachmentS.all()
+    res.json(attachments)
+  }
