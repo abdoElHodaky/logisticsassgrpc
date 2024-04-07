bef00a097f66f427a3458e9b1df1981355dfaf64
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 10:16:03 +0200

    Update users.ts

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,1 +1,35 @@
+import { Injectable , Inject } from "@decorators/di";
+import { DataSource } from "typeorm"
+import { Article } from "../entity/Article"
+import { Author } from "../entity/Author"
+import { User } from "../entity/User";
+import { AppDataSource , _datasourceInject } from "../data-source";
+import { CreateArticleDto } from "../dto/create-article.dto"
 
+@Injectable()
+export class UserService {
+  constructor (@Inject(_datasourceInject) private readonly datasource: DataSource){}
+
+  async all():Promise<User>{
+    return await this.datasource.manager.find(User)
+  }
+  
+  async id(userId:string):Promise<User>{
+
+
+    if(isNumeric(userId)==true){
+      console.log(nationalIdvalid(userId))
+      const _id=Number(userId)
+     let user=await this.datasource.getRepository(User).findOneOrFail({
+        where:{
+          id:_id
+        },
+        relations:{
+          tickets:true,
+          verifications:true
+        }
+      })
+      return user
+  }
+  
+}
