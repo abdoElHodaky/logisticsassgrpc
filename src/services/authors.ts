e24560ef5ace66a4e3f2b58decd14993eb851cc0
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 13:09:26 +0200

    Update authors.ts

--- src/services/authors.ts
+++ src/services/authors.ts
@@ -1,1 +1,38 @@
+import { Injectable , Inject } from "@decorators/di";
+import { Article,User,Author } from "../entity/"
+import { DataSource ,AppDataSource  } from "../includes"
+import { CreateArticleDto } from "../dto/create-article.dto"
+import { isNumeric,nationalIdvalid } from "../helpers";
 
+@Injectable()
+export class UserService {
+  
+  public  datasource:DataSource=AppDataSource
+  constructor ( ){}
+
+  async all():Promise<Author[]>{
+   // console.log(this.datasource)
+    return await this.datasource.manager.find(Author)
+  }
+  
+  async id(userId:string):Promise<User|void> {
+
+
+    if(isNumeric(userId)==true){
+      console.log(nationalIdvalid(userId))
+      const _id=Number(userId)
+     let user=await this.datasource.getRepository(User).findOneOrFail({
+        where:{
+          id:_id
+        },
+        relations:{
+          tickets:true,
+          verifications:true
+        }
+      })
+      return user
+    }
+    else return 
+  }
+  
+}
