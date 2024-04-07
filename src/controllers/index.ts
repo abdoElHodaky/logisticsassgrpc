c3767757a66ee2986cd6cb53e1acfd52ebb278b8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 17:11:08 +0200

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,7 @@
 export {AuthController} from "./auths";
+export {AuthorController} from "./authors";
+export {UserController} from "./users";
+export {UserTicketController} from "./users.tickets";
+export {ArticleController} from "./articles";
+
+
