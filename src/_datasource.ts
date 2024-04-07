901486e7625980c44531c15da9bd9141a576c971
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 16:08:47 +0200

    Update _datasource.ts

--- src/_datasource.ts
+++ src/_datasource.ts
@@ -1,7 +1,7 @@
 import { getDataSource,_AppDataSource } from "./data-source"
 import { DataSource } from "typeorm"
 export let AppDataSource:DataSource=_AppDataSource
-export { _datasourceInject } from "./data-source"
+export { _datasource } from "./data-source"
 
  /*getDataSource(1000).then(d=>{
   AppDataSource=d
