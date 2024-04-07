444452201848a6976fb83f17099ca0e057e338d6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 18:11:30 +0200

    Update authors.ts

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -1,7 +1,7 @@
 import { AuthorService} from "../services/";
 import { Article , Author } from "../entity/"
 import { AppDataSource } from "../_datasource";
-import { CreateAuthorDto } from "../dto/create-author.dto"
+import { CreateAuthorDto } from "../dto/"
 import { Res, Post, Controller, Get, Body , Params ,Delete } from '@decorators/express';
 import { Response ,Request} from "express"
 import { isNumeric,nationalIdvalid } from "../helpers";
@@ -24,7 +24,7 @@ export class AuthorController {
 
   @Post("/")
   async create(@Body() author:CreateAuthorDto):Promise<Author>{
-    let _author=<Author>{...author}
+    let _author=author
    _author=await AppDataSource.getRepository(Author).save(author)
     return _author
   }
