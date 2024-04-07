b13e4a995d9b87fd62887fc0c35aa15a2f3c4692
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 19:11:13 +0200

    Update authors.ts

--- src/services/authors.ts
+++ src/services/authors.ts
@@ -35,7 +35,7 @@ export class AuthorService {
    // else return 
   }
 
-  async create(author:CreateAuthorDto):Promise<Author>{
+  async create(author:Author):Promise<Author>{
     let _author;
    _author=await this.datasource.getRepository(Author).create(author)
     return _author
