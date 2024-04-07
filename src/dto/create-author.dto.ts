f3c91688d92cf0890a7e5790b94b265c2c5ec8f7
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 20:24:23 +0200

    Update create-author.dto.ts

--- src/dto/create-author.dto.ts
+++ src/dto/create-author.dto.ts
@@ -8,17 +8,5 @@ export class CreateAuthorDto{
   readonly type?:string;
   readonly passwordHash?:string;
   
-  to_ob(){
-    return {
-      username:this.username,
-      firstname:this.firstname,
-      lastname:this.lastname,
-      IDcardNumber:this.IDcardNumber,
-      email:this.email,
-      age:this.age,
-      type:this.type,
-      passwordHash:this.passwordHash
-    }
-  }
 }
 
