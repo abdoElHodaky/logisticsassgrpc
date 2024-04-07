1fa981a4deb03e47dfcaac38c45ee72ab965721b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 17:31:27 +0200

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -51,7 +51,7 @@ apiv1.post("/suptickets/create",(req,res)=>{
 
 })
 console.log(controllers)
-attachControllers(apiv1,[controllers])
+attachControllers(apiv1,[...controllers])
 //apiv1.use(authroute)
 //apiv1.use(usersroute);
 //attachControllers(apiv1,[UserController,UserTicketController ])
