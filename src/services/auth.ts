702abab15467c47b1f35f009f93bc2208d2da543
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 15:10:57 +0200

    Update auth.ts

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,1 +1,23 @@
+import { Injectable , Inject } from "@decorators/di";
+import { User } from "../entity/"
+import { DataSource ,AppDataSource  } from "../includes"
+import { CreateUserDto } from "../dto/create-user.dto"
+import { isNumeric,nationalIdvalid } from "../helpers";
 
+//@Injectable()
+export class AuthService {
+  
+  public  datasource:DataSource=AppDataSource
+  constructor ( ){}
+
+  async login():Promise<User>{
+    
+  }
+  
+  async create():Promise<User|void> {
+
+
+    
+  }
+  
+}
