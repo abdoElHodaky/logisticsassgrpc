9870bc8ca9577557503498d42e88f471eb8be0a5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 17:28:06 +0200

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,13 @@
-export {AuthController} from "./auths";
-export {AuthorController} from "./authors";
-export {UserController} from "./users";
-export {UserTicketController} from "./users.tickets";
-export {ArticleController} from "./articles";
-export {AttachmentController} from "./attachments";
+import {AuthController} from "./auths";
+import {AuthorController} from "./authors";
+import {UserController} from "./users";
+import {UserTicketController} from "./users.tickets";
+import {ArticleController} from "./articles";
+import {AttachmentController} from "./attachments";
 
+
+export const controllers=[AuthController,
+                          AuthorController,
+                          UserController
+                          UserTicketController,
+                          AttachmentController]
