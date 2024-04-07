e018cb506c499c379052749aa8d1a42389fd7cf3
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 13:58:45 +0200

    Update users.tickets.ts

--- src/services/users.tickets.ts
+++ src/services/users.tickets.ts
@@ -1,1 +1,45 @@
 
+import { AppDataSource , DataSource } from "../_datasource";
+//import { CreateArticleDto } from "../dto/create-article.dto"
+import { Res, Post, Controller, Get, Body , Params } from '@decorators/express';
+import { Response ,Request} from "express"
+import { supTicket ,User } from "../entity/";
+
+
+export class UserTicketService {
+
+  
+  async all(id:string):Promise<supTicket|void>
+  {
+    
+    let id=Number(userid)
+    let user=await AppDataSource.manager.findOneOrFail(User,{where:{
+            id:id
+           },
+           relations:{
+            tickets:true
+           }
+            })
+    let tickets=user.tickets
+    res.json(tickets)
+  }
+  
+  @Post("/:userid/tickets")
+  async create(@Params("userid") userid:string){
+    
+    
+    let id=Number(userid)
+    let supticket=new supTicket()
+    let user:User;
+    let ticket:supTicket;
+    supticket.type="inquiry"
+    supticket.subject="Greet"
+    supticket.description="How are you?"
+    user=await AppDataSource.manager.findOneByOrFail(User,{id:id})
+    user.tickets.push(supticket)
+    await AppDataSource.manager.save(User,user)
+    return {message:"created success"}
+    
+  }
+  
+}
