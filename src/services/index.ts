d9ef2ec8c50ea5c4c07fbbd39ddebca412dfb9d7
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 15:25:40 +0200

    Create index.ts

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import {AuthService} from "./auth";
+import {UserService} from "./users";
+import {AuthorService} from "./authors";
+import {ArticleService} from "./articles";
+import {UserTicketService} from "./auth";
