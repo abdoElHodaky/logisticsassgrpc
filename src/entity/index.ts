a814445258c2bcceae58c6b39e905799653c8e47
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-07 10:31:22 +0200

    Update index.ts

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,7 +1,7 @@
 export { Article } from "./Article"
 export { User } from "./User"
 export { Author } from "./Author"
-export { SupTicket } from "./SupTicket"
+export { SupTicket } from "./supTicket"
 export { Attachment } from "./Attachment"
 export { Boom } from "./Book"
 
