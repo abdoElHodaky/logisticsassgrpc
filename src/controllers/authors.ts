8e1b531df58e93b1bd50cf20d4c9c910313fb071
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-08 09:24:15 +0200

    Update authors.ts

--- src/controllers/authors.ts
+++ src/controllers/authors.ts
@@ -53,7 +53,8 @@ export class AuthorController {
        res.json({message:"user not found or you used invalid paramter"})
     }*/
     let author =await this.authorS.id(id)
-    return author
+    if (author instanceof Error){res.status(404).send(author["message"])}
+    else return author
    // else res.json({message:"user not found or you used invalid paramter"})
     
     
