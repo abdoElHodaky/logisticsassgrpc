f596b25e273d982efe1db57ed5939b3a1cef6ea8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-19 15:40:19 +0200

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,29 @@
-export {AuthService} from "./auth";
-export {AttachmentService} from "./attachments";
-export {UserService} from "./users";
-export {AuthorService} from "./authors";
-export {ArticleService} from "./articles";
-export {UserTicketService} from "./users.tickets";
+import "reflect-metadata";
+
+import {AuthService} from "./auth";
+import {AttachmentService} from "./attachments";
+import {UserService} from "./users";
+import {AuthorService} from "./authors";
+import {ArticleService} from "./articles";
+import {UserTicketService} from "./users.tickets";
+
+const services:any={
+  "Auth":AuthService,
+  "Author":AuthorService,
+  "User":UserService,
+  "Article":ArticleService,
+  "Attachment":AttachmentService,
+  "Ticket":UserTicketService,
+}
+
+export function service(service:string) {
+      return function (target: any, propertyKey?: string  ) {
+      Reflect.defineMetadata("service", service, target);
+      console.log(target)
+      let s=new services[service]()
+      if (target==null) return s
+      else  target["service"]=s
+      
+      
+  }
+}
