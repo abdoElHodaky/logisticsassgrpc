621bd0ce6d490d97159a43dc61c8f89fff5b5880
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-19 15:33:34 +0200

    Update payments.ts

--- src/services/payments.ts
+++ src/services/payments.ts
@@ -1,9 +1,11 @@
 import { _Data } from "./datasource";
 import { Payment,User} from "../entity/"
 import { CreatePaymentDto } from "../dto/create-payment.dto"
+import { PayTabService } from "../paytabs.service";
 
 //@Injectable()
 export class PaymentService extends _Data {
+  private payTabService:PayTabService
   constructor (){
       super()
   }
@@ -24,5 +26,11 @@ export class PaymentService extends _Data {
     _article=await this.datasource.manager.save(Article,_article)
     return _article*/
    
+ }
+
+async pay(paymentId:string,urls:{callback:string,return:string}){
+  let payment = await this.id(parseInt(paymentId))
+  return await this.payTabService.createPage(payment,urls)
+
  }
 }
