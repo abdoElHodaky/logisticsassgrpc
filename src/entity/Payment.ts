f9ba14046128641309731212122794378f68f9e5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-19 16:08:42 +0200

    Update Payment.ts

--- src/entity/Payment.ts
+++ src/entity/Payment.ts
@@ -1,1 +1,25 @@
+import { Entity,Column,ManyToOne,PrimaryGeneratedColumn} from "typeorm"
+import { User } from "./User";
 
+@Entity()
+export class Payment {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column({type: "varchar", nullable: true})
+    title: string;
+
+    @Column({type: "varchar"})
+    date: string;
+    
+    @Column({type: "varchar", nullable: true})
+    status: string;
+
+    @Column({type: "varchar", nullable: true})
+    amount: string;
+
+    @Column({type: "varchar", nullable: true})
+    currency: string;
+
+    @ManyToOne(()=>User,user=>user.payments) user:User;
+}
