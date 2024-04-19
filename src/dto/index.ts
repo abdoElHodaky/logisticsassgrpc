e6fe80e9530c879ab4b2537b56ff679b6d4e3c76
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-19 15:18:49 +0200

    Update index.ts

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -1,3 +1,4 @@
 export {CreateAuthorDto} from "./create-author.dto";
 export {CreateUserDto} from "./create-user.dto";
 export {LoginUserDto} from "./login-user.dto";
+export {CreatePurshaseDto,CreateSubscriptionDto} from "./create-purshase-subscribe.dto";
