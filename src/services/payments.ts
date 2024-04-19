63ff3264c2f50f872504cda444a878d355b4e9b2
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-19 15:11:23 +0200

    Update payments.ts

--- src/services/payments.ts
+++ src/services/payments.ts
@@ -1,1 +1,28 @@
+import { _Data } from "./datasource";
+import { Payment,User} from "../entity/"
+import { CreatePaymentDto } from "../dto/create-payment.dto"
 
+//@Injectable()
+export class PaymentService extends _Data {
+  constructor (){
+      super()
+  }
+
+  async all():Promise<Payment[]>
+  {
+    //console.log(this._source)
+    return await this.datasource.manager.find(Payment)
+  }
+
+ async create(createPaymentDto: CreatePaymentDto):Promise<Payment|void>{
+     
+    const {purshasedItem,userid}=createPaymentDto
+    /*let _article=<Article>{...article}
+    let author=await this.datasource.manager.findOneByOrFail(Author,{id:parseInt(userid)})
+    _article.author=author
+    author.articles.push(_article)
+    _article=await this.datasource.manager.save(Article,_article)
+    return _article*/
+   
+ }
+}
