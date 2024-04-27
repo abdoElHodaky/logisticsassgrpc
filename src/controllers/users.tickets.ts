25826a397a4cbad89d52701338f14be4b0985da0
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 01:28:17 +0300

    Update users.tickets.ts

--- src/controllers/users.tickets.ts
+++ src/controllers/users.tickets.ts
@@ -2,11 +2,11 @@
 import { Res, Post, Controller, Get, Body , Params } from '@decorators/express';
 import { Response ,Request} from "express"
 import { supTicket , User } from "../entity/";
-import { UserTicketService } from "../services/";
+import { services } from "../services/services";
 import { Error } from "common-errors";
 @Controller('/users')
 export class UserTicketController {
- private userticketS:UserTicketService =new UserTicketService()
+ private userticketS:UserTicketService =new services["Ticket"]()
   
  
   @Get("/:userid/tickets")
