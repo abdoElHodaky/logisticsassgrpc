3c1cc53a51166f13f6e4c0f741d595fab60f6f00
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 00:29:17 +0300

    Create auth.grpc.ts

---
+++ src/controllers/auth.grpc.ts
@@ -0,0 +1,29 @@
+import { credentials } from "@grpc/grpc-js";
+import {_Auth } from "../protos/dist/";
+import { Res,  Controller , Post ,Body } from "@decorators/express";
+import { Response  } from "express";
+import { LoginUserDto } from "../dto/";
+const address = "localhost:3030";
+
+@Controller("/grpc/auth")
+export class GrpcAuthController {
+  private client =new _Auth.AuthServiceClient(
+    address,
+    credentials.createInsecure()
+  )
+  
+  @Post("login")
+  async login(@Res() res:Response, @Body() loginUserDto:LoginUserDto ):Promise<void>{
+    const req:_Auth.LoginUserReq={
+      ...loginUserDto
+    }
+    this.client.login(req,(err:any,resp:_Auth.LoginUserRes)=>{
+      if (err) {
+      res.jsonp(err);
+        console.error(err)
+    } else {
+       res.json(resp)
+     }
+    })
+  }
+}
