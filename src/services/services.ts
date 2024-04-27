e91461f40dd31659afb9dee4f1980f2c7386ccae
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 01:41:00 +0300

    Update services.ts

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,16 +1,25 @@
 
-import * as Services from "./";
-let servkey = Symbol("servname");
+import {AuthService} from "./auth";
+import {AttachmentService} from "./attachments";
+import {UserService} from "./users";
+import {AuthorService} from "./authors";
+import {ArticleService} from "./articles";
+import {UserTicketService} from "./users.tickets";
+import {ArticleGrpcService} from "./articles.grpc";
+import {TicketGrpcService} from "./tickets.grpc";
+import {AuthGrpcService} from "./auth.grpc";
+import {AuthorGrpcService} from "./authors.grpc";
+
 export let services:any={
-  "Auth":Services.AuthService,
-  "Author":Services.AuthorService,
-  "User":Services.UserService,
-  "Article":Services.ArticleService,
-  "Attachment":Services.AttachmentService,
-  "Ticket":Services.UserTicketService,
-  "Grpc.Ticket":Services.TicketGrpcService,
-  "Grpc.Article":Services.ArticleGrpcService,
-  "Grpc.Auth":Services.AuthGrpcService,
-  "Grpc.Author":Services.AuthorGrpcService
+  "Auth":AuthService,
+  "Author":AuthorService,
+  "User":UserService,
+  "Article":ArticleService,
+  "Attachment":AttachmentService,
+  "Ticket":UserTicketService,
+  "Grpc.Ticket":TicketGrpcService,
+  "Grpc.Article":ArticleGrpcService,
+  "Grpc.Auth":AuthGrpcService,
+  "Grpc.Author":AuthorGrpcService
 
 }
