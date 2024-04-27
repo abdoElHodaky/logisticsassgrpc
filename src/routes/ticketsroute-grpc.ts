a1cef0363a5998d1634ce0a2cc997167bc2d2d03
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 00:25:02 +0300

    Create ticketsroute-grpc.ts

---
+++ src/routes/ticketsroute-grpc.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+//import { Author } from "../entity/Author";
+//import { Article } from "../entity/Article"
+//import { AppDataSource } from "../_datasource";
+
+export const ticketsgrpc=Router()
+
+ticketsgrpc.get("/grpc/users/:userId/tickets",(req,res)=>{
+    /* 	#swagger.tags = ['GRPC.User.Ticket']
+        #swagger.description = 'Endpoint to get articles via grpc' 
+        #swagger.parameters['userId'] = {
+            in: 'path',
+            description: 'Add new ticket.',
+            schema: { $ref: '#/definitions/userSupTicket' }
+    }
+
+    */
+})
+
+    
+
+ticketsgrpc.post("/grpc/users/:userId/tickets",({body},res)=>{
+    /* 	#swagger.tags = ['GRPC.User.Ticket']
+        #swagger.description = 'Endpoint to add new ticket via grpc' 
+        #swagger.parameters['userId'] = {
+            in: 'path',
+            description: 'Add new ticket.',
+            schema: { $ref: '#/definitions/userSupTicket' }
+    }
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'Add new ticket.',
+            schema: { $ref: '#/definitions/userAddTicket' }
+    } */
+    
+   
+})
+
+/*articlesroute.post("/articles/create",(req,res)=>{
+    
+})
+*/
