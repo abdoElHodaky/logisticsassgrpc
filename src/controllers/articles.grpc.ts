2fe80bd9da0208a1954959099ce8860635e6a9e2
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 00:27:33 +0300

    Create articles.grpc.ts

---
+++ src/controllers/articles.grpc.ts
@@ -0,0 +1,27 @@
+import { credentials } from "@grpc/grpc-js";
+import {_Article } from "../protos/dist/";
+import { Res,  Controller , Get } from "@decorators/express";
+import { Response  } from "express";
+
+const address = "localhost:3030";
+
+@Controller("/grpc/articles")
+export class GrpcArticleController {
+  private client =new _Article.ArticleServiceClient(
+    address,
+    credentials.createInsecure()
+  )
+  
+  @Get("")
+  async all(@Res() res:Response ):Promise<void>{
+    const req:_Article.GetAllReq={}
+    this.client.all(req,(err:any,resp:_Article.GetAllRes)=>{
+      if (err) {
+      res.jsonp(err);
+        console.error(err)
+    } else {
+       res.json(resp)
+     }
+    })
+  }
+}
