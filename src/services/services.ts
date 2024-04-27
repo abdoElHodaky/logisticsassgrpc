634b1e5f5ad48e166d8e811e254739c16fd1c12c
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 00:07:30 +0300

    Create services.ts

---
+++ src/services/services.ts
@@ -0,0 +1,16 @@
+
+import * as Services from "./";
+let servkey = Symbol("servname");
+export let services:any={
+  "Auth":Services.AuthService,
+  "Author":Services.AuthorService,
+  "User":Services.UserService,
+  "Article":Services.ArticleService,
+  "Attachment":Services.AttachmentService,
+  "Ticket":Services.UserTicketService,
+  "Grpc.Ticket":Services.TicketGrpcService,
+  "Grpc.Article":Services.ArticleGrpcService,
+  "Grpc.Auth":Services.AuthGrpcService,
+  "Grpc.Author":Services.AuthorGrpcService
+
+}
