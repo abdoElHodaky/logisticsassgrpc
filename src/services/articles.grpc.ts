c37700bf62173121296bf08f6c63987d2b377a85
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 00:05:33 +0300

    Create articles.grpc.ts

---
+++ src/services/articles.grpc.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata";
+import { sendUnaryData, ServerUnaryCall, status, UntypedHandleCall ,handleUnaryCall} from "@grpc/grpc-js";
+import  {_Article} from "../protos/dist/";
+//import { Service} from "./service.decorator";
+import { services } from "./services";
+export class ArticleGrpcService  {
+  static service:any=new services["Article"]()
+  //public [name: string]:UntypedHandleCall;
+  public SrvImpl: _Article.ArticleServiceServer = {
+   async all (call: ServerUnaryCall<_Article.GetAllReq,_Article.GetAllRes>,
+    callback: sendUnaryData<_Article.GetAllRes>
+ ){
+     let articles=await ArticleGrpcService.service.all()
+     let _articles=articles.map(_Article.Article.fromJSON)
+     let res:_Article.GetAllRes={articles:_articles,error:{
+       Message:"",type:"",name:""
+     }}
+     callback({code:status.OK},res)
+      }
+    , async create (
+    call: ServerUnaryCall<_Article.CreateReq,_Article.CreateRes>,
+    callback: sendUnaryData<_Article.CreateRes>
+  ){
+       
+     }}
+
+  
+    
+/*static  async _all (call: ServerUnaryCall<_Article.GetAllReq,_Article.GetAllRes>,
+    callback: sendUnaryData<_Article.GetAllRes>
+ ){
+  let articles=await this.artclS.all()
+  let _articles=articles.map(_Article.Article.fromJSON)
+  console.log(_articles)
+  }
+
+static  async _create(
+    call: ServerUnaryCall<_Article.CreateReq,_Article.CreateRes>,
+    callback: sendUnaryData<_Article.CreateRes>
+  ){}
+    */
+ }
+console.log(services)
+//console.log(Reflect.getMetadata("servname",ArticleGrpcService.service ))
