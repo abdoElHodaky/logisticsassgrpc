1fa0fae2bce6fa5bc0843dc6056cc725847fdf96
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 01:45:09 +0300

    Update grpc.ts

--- src/grpc.ts
+++ src/grpc.ts
@@ -8,7 +8,7 @@ const HOST =  "0.0.0.0";
 const PORT =  "50051";
 const server = new Server()
 const address = `${HOST}:${PORT}`;
-//console.log(services)
+console.log(services)
 server.addService(_Article.ArticleServiceService,new services["Grpc.Article"]().SrvImpl)
 server.addService(_Ticket.TicketServiceService,new services["Grpc.Ticket"]().SrvImpl)
 server.addService(_Auth.AuthServiceService,new services["Grpc.Auth"]().SrvImpl)
