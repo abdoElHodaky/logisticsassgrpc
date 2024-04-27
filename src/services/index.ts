5e7b3e3e97c266a4ac8d644d53ab8b88288a642c
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 00:09:14 +0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,12 @@
+
+
 export {AuthService} from "./auth";
 export {AttachmentService} from "./attachments";
 export {UserService} from "./users";
 export {AuthorService} from "./authors";
 export {ArticleService} from "./articles";
 export {UserTicketService} from "./users.tickets";
+export {ArticleGrpcService} from "./articles.grpc";
+export {TicketGrpcService} from "./tickets.grpc";
+export {AuthGrpcService} from "./auth.grpc";
+export {AuthorGrpcService} from "./authors.grpc";
