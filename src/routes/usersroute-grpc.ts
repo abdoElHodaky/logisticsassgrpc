8e9ca42df81d0aee70c14afd6f0d0b6dbbef187a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 14:56:23 +0300

    Update usersroute-grpc.ts

--- src/routes/usersroute-grpc.ts
+++ src/routes/usersroute-grpc.ts
@@ -1,1 +1,31 @@
+import { Router } from "express";
+//import { Author } from "../entity/Author";
+//import { Article } from "../entity/Article"
+//import { AppDataSource } from "../_datasource";
 
+export const usersgrpc=Router()
+
+usersgrpc.get("/grpc/users",(req,res)=>{
+    /* 	#swagger.tags = ['GRPC.User']
+        #swagger.description = 'Endpoint to get authors via grpc' */
+
+})
+
+    
+
+usersgrpc.post("/grpc/users",({body},res)=>{
+    /* 	#swagger.tags = ['GRPC.User']
+        #swagger.description = 'Endpoint to add new User via grpc' 
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'Add new user.',
+            schema: { $ref: '#/definitions/CreateAuthor' }
+    } */
+    
+   
+})
+
+/*articlesroute.post("/articles/create",(req,res)=>{
+    
+})
+*/
