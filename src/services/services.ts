b4e847ceec22af20edf465bb44d3c4a7a1da3c57
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 09:47:23 +0300

    Update services.ts

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,19 +1,15 @@
 import * as Services from "./";
 let servkey = Symbol("servname");
-export let services:any={
-  "Auth":Services.AuthService,
-  "Author":Services.AuthorService,
-  "User":Services.UserService,
-  "Article":Services.ArticleService,
-  "Attachment":Services.AttachmentService,
-  "Ticket":Services.UserTicketService,
-  "Grpc.Ticket":Services.TicketGrpcService,
-  "Grpc.Article":Services.ArticleGrpcService,
-  "Grpc.Auth":Services.AuthGrpcService,
-  "Grpc.Author":Services.AuthorGrpcService
 
-}
-
-export const Service=(servname:string)=>{
-   return new services[servname]()
+export enum services{
+  Auth=Services.AuthService,
+  Author=Services.AuthorService,
+  User=Services.UserService,
+  Article=Services.ArticleService,
+  Attachment=Services.AttachmentService,
+  Ticket=Services.UserTicketService,
+  Grpc_Ticket:=ervices.TicketGrpcService,
+  Grpc_Article=Services.ArticleGrpcService,
+  Grpc_Auth=Services.AuthGrpcService,
+  Grpc_Author.AuthorGrpcService
 }
