a87d46d3027f5633583d789e034b29ffedcec56f
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 10:13:08 +0300

    Update articles.grpc.ts

--- src/services/articles.grpc.ts
+++ src/services/articles.grpc.ts
@@ -2,10 +2,10 @@ import "reflect-metadata";
 import { sendUnaryData, ServerUnaryCall, status, UntypedHandleCall ,handleUnaryCall} from "@grpc/grpc-js";
 import  {_Article} from "../protos/dist/";
 //import { Service} from "./service.decorator";
-import { services }from "./enum";
+import { ArticleService }from "./";
 console.log(services)
 export class ArticleGrpcService  {
-  static service:any=services.Article
+  static service:any=new ArticleService()
   //public [name: string]:UntypedHandleCall;
   public SrvImpl: _Article.ArticleServiceServer = {
    async all (call: ServerUnaryCall<_Article.GetAllReq,_Article.GetAllRes>,
