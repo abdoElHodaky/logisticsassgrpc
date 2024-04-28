d975a06adb476bfcf46ced61eb8679f56e53815e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 12:32:29 +0300

    Update grpc.ts

--- src/grpc.ts
+++ src/grpc.ts
@@ -6,7 +6,7 @@ import { _Article,_Ticket,_Auth ,_User} from "./protos/dist/";
 import { services } from "./services/enum";
 const HOST =  "0.0.0.0";
 const PORT =  "50051";
-const server = new Server()
+export const server = new Server()
 const address = `${HOST}:${PORT}`;
 //console.log(services)
 server.addService(_Article.ArticleServiceService,services.Grpc_Article.SrvImpl)
@@ -18,4 +18,3 @@ server.addService(_User.UserServiceService,services.Grpc_Author.SrvImpl)
 /*server.bindAsync('localhost:50051', ServerCredentials.createInsecure(), () => {
     console.log("Server started")
     server.start(); })*/
-export  server
