11907862bbb561de0b0530e736303a22d43c3e48
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-29 00:02:27 +0300

    Update grpc.ts

--- src/grpc.ts
+++ src/grpc.ts
@@ -7,15 +7,21 @@ import { services } from "./services/enum";
 const HOST =  "0.0.0.0";
 const PORT =  "50051";
 export const server = new Server()
+export const server2 = new Server()
 const address = `${HOST}:${PORT}`;
 //console.log(services)
 server.addService(_Article.ArticleServiceService,services.Grpc_Article.SrvImpl)
 server.addService(_Ticket.TicketServiceService,services.Grpc_Ticket.SrvImpl)
 server.addService(_Auth.AuthServiceService,services.Grpc_Auth.SrvImpl)
 server.addService(_User.UserServiceService,services.Grpc_Author.SrvImpl,services.Grpc_User.SrvImpl)
-//server.addService(_User.UserServiceService,services.Grpc_User.SrvImpl)
+server2.addService(_User.UserServiceService,services.Grpc_User.SrvImpl)
 
 
 server.bindAsync('localhost:50051', ServerCredentials.createInsecure(), () => {
     console.log("Server started")
     server.start(); })
+
+server2.bindAsync('localhost:3030', ServerCredentials.createInsecure(), () => {
+    console.log("Server2 started")
+    server.start(); })
+
