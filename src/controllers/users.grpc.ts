08aaebb79f90a96d941aaf7d0001f24695786325
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 14:51:11 +0300

    Update users.grpc.ts

--- src/controllers/users.grpc.ts
+++ src/controllers/users.grpc.ts
@@ -1,1 +1,27 @@
+import { credentials } from "@grpc/grpc-js";
+import {_User } from "../protos/dist/";
+import { Res,  Controller , Get } from "@decorators/express";
+import { Response  } from "express";
 
+const address = "localhost:50051";
+
+@Controller("/grpc/users")
+export class GrpcAuthorController {
+  private client =new _User.UserServiceClient(
+    address,
+    credentials.createInsecure()
+  )
+  
+  @Get("")
+  async all(@Res() res:Response ):Promise<void>{
+    const req:_User.GetAllUserReq={}
+    this.client.all(req,(err:any,resp:_User.GetAllUserRes)=>{
+      if (err) {
+      res.jsonp(err);
+        console.error(err)
+    } else {
+       res.json(resp)
+     }
+    })
+  }
+}
