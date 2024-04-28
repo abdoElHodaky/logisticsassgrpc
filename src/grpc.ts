5b72dd151963f187d87be258bf41a89c8b8d4521
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 10:00:42 +0300

    Update grpc.ts

--- src/grpc.ts
+++ src/grpc.ts
@@ -3,16 +3,16 @@ import { Server, ServerCredentials } from "@grpc/grpc-js";
 
 import { _Article,_Ticket,_Auth ,_User} from "./protos/dist/";
 //import { _Ticket  } from "./protos/dist/"
-import {services}  from "./services/services";
+import {services}  from "./services/";
 const HOST =  "0.0.0.0";
 const PORT =  "50051";
 const server = new Server()
 const address = `${HOST}:${PORT}`;
 console.log(services)
-server.addService(_Article.ArticleServiceService,new services["Grpc.Article"]().SrvImpl)
-server.addService(_Ticket.TicketServiceService,new services["Grpc.Ticket"]().SrvImpl)
-server.addService(_Auth.AuthServiceService,new services["Grpc.Auth"]().SrvImpl)
-server.addService(_User.UserServiceService,new services["Grpc.Author"]().SrvImpl)
+server.addService(_Article.ArticleServiceService,services.Grpc_Article.SrvImp)
+server.addService(_Ticket.TicketServiceService,services.Grpc_Ticket.SrvImp)
+server.addService(_Auth.AuthServiceService,services.Grpc_Auth.SrvImpl)
+server.addService(_User.UserServiceService,services.Grpc_Author.SrvImp)
 
 
 server.bindAsync('localhost:3030', ServerCredentials.createInsecure(), () => {
