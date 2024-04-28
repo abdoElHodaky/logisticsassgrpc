5475f2e2143a64e1f92a02a9ec7a4f05a2720f16
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-28 12:12:26 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -42,11 +42,16 @@ app.get("/",(req,res)=>{
 })
 app.use('/endpoints', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  server.bindAsync('localhost:50051', ServerCredentials.createInsecure(), () => {
+const configureApplication =  () => {
+   server.bindAsync('localhost:50051', ServerCredentials.createInsecure(), () => {
     console.log("Server started")
     server.start(); })
-})
+   app.listen(port, () => {
+	   console.log(`Example app listening on port ${port}`)
+
+  });
+}
+
+configureApplication();
 
 module.exports = app;
