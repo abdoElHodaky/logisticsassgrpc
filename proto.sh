e0c19958def71967a55c9c0e1dfb085b56c5cb83
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-29 12:57:43 +0300

    Update proto.sh

--- proto.sh
+++ proto.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#sudo ln -s "$(which node)" /usr/bin/node
-#sudo ln -s "$(which npm)" /usr/bin/npm
+sudo ln -s "$(which node)" /usr/bin/node
+sudo ln -s "$(which npm)" /usr/bin/npm
 
 sudo protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto  --ts_proto_out="src/protos/dist"  --ts_proto_opt=outputServices=grpc-js --ts_proto_opt=esModuleInterop=true -I=src/protos/src src/protos/src/*.proto --experimental_allow_proto3_optional
