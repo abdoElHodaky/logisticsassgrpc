2f58ce5818301de90e904a3a4a32b34dc3bda27e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-29 16:59:29 +0300

    Update authors.grpc.ts

--- src/services/authors.grpc.ts
+++ src/services/authors.grpc.ts
@@ -27,6 +27,9 @@ export class AuthorGrpcService  {
      
      let _authors=authors.map(_User.User.fromJSON)
      // console.log(_authors)
+      _authors.forEach(({id,articles},inx)=>{
+        if(articles.length!=0) articles=articles.map(a.userId=id)
+      })
       const res:_User.GetAllUserRes={
           users:_authors,
           error:{
