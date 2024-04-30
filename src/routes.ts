3e74cb32dc79e6e0bd0e0cc095b7a13e7daab0c5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-30 17:12:14 +0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,59 +1,10 @@
 import { Router } from "express";
 import { attachControllers } from '@decorators/express'
 import { controllers } from "./controllers/";
-//import { supTicket } from "./entity/supTicket";
-//import { User } from "./entity/User";
-//import { AppDataSource } from "./_datasource";
 import { attachmentsroute } from "./routes/attachmentroutes";
 import { authorsgrpc } from "./routes/authorsroute-grpc";
 
 export const apiv1=Router();
-apiv1.get("/grpc/suptickets",(req,res)=>{
-        console.log(req)
-/* 	#swagger.tags = ['GRPC.suptickets']
-
-        #swagger.description = 'Endpoint to get tickets' 
-    */
-    
-  /*  AppDataSource.manager.find(supTicket).
-    then(d=>{
-        //d.map((el,i)=>console.log(el.user))
-        res.json(d)
-    }).catch(console.log)*/
-})
-apiv1.post("/grpc/suptickets",(req,res)=>{
-    /* 	#swagger.tags = ['GRPC.suptickets']
-        #swagger.description = 'Endpoint to create tickets for specific user' 
-        #swagger.parameters['userid'] = {
-            in: 'body',
-            description: 'userid',
-            schema: { $ref: '#/definitions/userAddTicket' }
-    } 
-    */
-    
-   /* let supticket:supTicket=<supTicket>{...req.body.ticket}
-    let userid=req.body.userid
-    let user:User;
-    AppDataSource.manager.findOneByOrFail(User,{id:userid}).then(d=>{
-        user=d;
-        return user
-    }).then(a=>{
-        supticket.user=a;
-        a.tickets=[]
-        a.tickets.push(supticket)
-        return supticket
-    }).then(a=>{
-        AppDataSource.manager.save(supTicket,a)
-        //AppDataSource.manager.save(a.user)
-        res.json({message:"created successfully"})
-    })
-    .catch(console.log);*/
-    /*AppDataSource.manager.save(supTicket,ticket).then(d=>{
-        res.json({message:"created successfully"});
-    }).catch(console.log);*/
-    
-
-})
 //console.log(controllers)
 attachControllers(apiv1,[...controllers])
 //apiv1.use(authroute)
