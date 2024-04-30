6ba32fe2c6e49042d9a33a697f3f280b2982ed63
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-30 16:42:48 +0300

    Update payments.ts

--- src/services/payments.ts
+++ src/services/payments.ts
@@ -9,7 +9,7 @@ export class PaymentService extends _Data {
   private payTabService=new PayTabService()
   constructor (){
       super()
-      this.payTabService.start()
+     // this.payTabService.start()
   }
 
   async all():Promise<Payment[]>
