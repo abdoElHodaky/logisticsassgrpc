12b92e839424a115472853ee2bd93a8971ddbe43
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-30 17:14:14 +0300

    Create supticketsroute-grpc.ts

---
+++ src/routes/supticketsroute-grpc.ts
@@ -0,0 +1,48 @@
+import { Router } from "express";
+export const supticketsroute=Router()
+supticketsroute.get("/grpc/suptickets",(req,res)=>{
+        console.log(req)
+/* 	#swagger.tags = ['GRPC.suptickets']
+
+        #swagger.description = 'Endpoint to get tickets' 
+    */
+    
+  /*  AppDataSource.manager.find(supTicket).
+    then(d=>{
+        //d.map((el,i)=>console.log(el.user))
+        res.json(d)
+    }).catch(console.log)*/
+})
+supticketsroute.post("/grpc/suptickets",(req,res)=>{
+    /* 	#swagger.tags = ['GRPC.suptickets']
+        #swagger.description = 'Endpoint to create tickets for specific user' 
+        #swagger.parameters['userid'] = {
+            in: 'body',
+            description: 'userid',
+            schema: { $ref: '#/definitions/userAddTicket' }
+    } 
+    */
+    
+   /* let supticket:supTicket=<supTicket>{...req.body.ticket}
+    let userid=req.body.userid
+    let user:User;
+    AppDataSource.manager.findOneByOrFail(User,{id:userid}).then(d=>{
+        user=d;
+        return user
+    }).then(a=>{
+        supticket.user=a;
+        a.tickets=[]
+        a.tickets.push(supticket)
+        return supticket
+    }).then(a=>{
+        AppDataSource.manager.save(supTicket,a)
+        //AppDataSource.manager.save(a.user)
+        res.json({message:"created successfully"})
+    })
+    .catch(console.log);*/
+    /*AppDataSource.manager.save(supTicket,ticket).then(d=>{
+        res.json({message:"created successfully"});
+    }).catch(console.log);*/
+    
+
+})
