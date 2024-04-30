57b7f5f856a83398d501fd2286d6e443cb024dcb
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-30 10:14:47 +0300

    Update suptickets.grpc.ts

--- src/controllers/suptickets.grpc.ts
+++ src/controllers/suptickets.grpc.ts
@@ -1,1 +1,29 @@
+import { credentials } from "@grpc/grpc-js";
+import {_Ticket } from "../protos/dist/";
+import { Res,  Controller , Get } from "@decorators/express";
+import { Response  } from "express";
 
+const address = "localhost:3030";
+
+@Controller("/grpc/suptickets")
+export class GrpcsupTicketController {
+  private client =new _Ticket.TicketServiceClient(
+    address,
+    credentials.createInsecure()
+  )
+  
+  @Get("")
+  async all(@Res() res:Response ):Promise<void>{
+    const req:_Ticket.GetAllReq={  
+      userId:0
+    }
+    this.client.all(req,(err:any,resp:_Ticket.GetAllRes)=>{
+      if (err) {
+      res.jsonp(err);
+        console.error(err)
+    } else {
+       res.json(resp)
+     }
+    })
+  }
+}
