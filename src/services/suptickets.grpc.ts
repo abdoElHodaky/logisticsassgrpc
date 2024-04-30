54b24a4a4a541534075bf6d07714b493d750c13a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-04-30 17:51:35 +0300

    Update suptickets.grpc.ts

--- src/services/suptickets.grpc.ts
+++ src/services/suptickets.grpc.ts
@@ -17,8 +17,7 @@ export class supTicketGrpcService  {
      let _tickets=tickets.map(_Ticket.Ticket.fromJSON)
      //console.log(tickets)
     _tickets.forEach((a,inx)=>{   
-       console.log(a.userId,tickets[inx].user)
-      // _tickets[inx].userId=tickets[inx].user.id
+     if(a!=null) a.userId=tickets[inx].user.id
      })
      let res:_Ticket.GetAllTicketRes={tickets:_tickets,error:{
        Message:"",type:"",name:""
