9b7586cccbcb45fe0e597f748bcfe908addfbc82
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-01 18:15:29 +0300

    Update grpc-reflect.ts

--- src/grpc-reflect.ts
+++ src/grpc-reflect.ts
@@ -18,3 +18,26 @@ protoLoader.load("src/protos/src/user.proto").then(pkgd=>{
   reflection.addToServer(server2);
     //console.log(reflection)
 }).catch(console.log)
+
+
+protoLoader.load("src/protos/src/ticket.proto").then(pkgd=>{
+//console.log(pkgd)
+  const reflection = new ReflectionService(loadPackageDefinition(pkgd));
+  reflection.addToServer(server);
+    //console.log(reflection)
+}).catch(console.log)
+
+
+protoLoader.load("src/protos/src/user.proto").then(pkgd=>{
+//console.log(pkgd)
+  const reflection = new ReflectionService(loadPackageDefinition(pkgd));
+  reflection.addToServer(server);
+    //console.log(reflection)
+}).catch(console.log)
+
+protoLoader.load("src/protos/src/auth.proto").then(pkgd=>{
+//console.log(pkgd)
+  const reflection = new ReflectionService(loadPackageDefinition(pkgd));
+  reflection.addToServer(server);
+    //console.log(reflection)
+}).catch(console.log)
