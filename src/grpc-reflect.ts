84d924919d4c4ced9b9b9ae7f8a4e4dda6eb803b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-01 21:47:07 +0300

    Update grpc-reflect.ts

--- src/grpc-reflect.ts
+++ src/grpc-reflect.ts
@@ -4,8 +4,16 @@ import { loadPackageDefinition }  from "@grpc/grpc-js";
 import { ReflectionService } from '@grpc/reflection';
 const protoLoader = require('@grpc/proto-loader');
 console.log(protoLoader,ReflectionService)
-protoLoader.load("./src/protos/src/ticket.proto")
-protoLoader.load("./src/protos/src/article.proto")
-protoLoader.load("./src/protos/src/user.proto")
-protoLoader.load("./src/protos/src/auth.proto")
-console.log(protoLoader)
+async start(){
+let pkgticket=await protoLoader.load("./src/protos/src/ticket.proto")
+let pkgarticle=await protoLoader.load("./src/protos/src/article.proto")
+let pkguser=await protoLoader.load("./src/protos/src/user.proto")
+let pkgauth=await protoLoader.load("./src/protos/src/auth.proto")
+
+const reflect=new ReflectionService(pkgticket,pkgarticle)
+  reflect.addToServer(server)
+}
+
+server 
+server2
+start()
