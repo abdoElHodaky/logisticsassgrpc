493f9d3f07f3f11b4bd6daaf4e41444a8056e32e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-01 20:53:41 +0300

    Update grpc-reflect.ts

--- src/grpc-reflect.ts
+++ src/grpc-reflect.ts
@@ -3,8 +3,8 @@ import {server,server2} from "./grpc";
 import { loadPackageDefinition }  from "@grpc/grpc-js";
 import { ReflectionService } from '@grpc/reflection';
 const protoLoader = require('@grpc/proto-loader');
-console.log(protoLoader)
-protoLoader.load("src/protos/src/ticket.proto").then(pkgd=>{
+console.log(protoLoader,ReflectionService)
+protoLoader.load("src/protos/src/ticket.proto").then((pkgd:any)=>{
   console.log(pkgd)
   //const reflection = new ReflectionService(pkgd);
  // reflection.addToServer(server2);
