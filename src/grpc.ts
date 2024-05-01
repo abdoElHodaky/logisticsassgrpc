adb339c68914d93b0f5f1a5e590e984e50df25cc
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-01 22:58:40 +0300

    Update grpc.ts

--- src/grpc.ts
+++ src/grpc.ts
@@ -16,7 +16,7 @@ server.addService(_User.UserServiceService,services.Grpc_Author.SrvImpl)
 server2.addService(_User.UserServiceService,services.Grpc_User.SrvImpl)
 server2.addService(_Ticket.TicketServiceService,services.Grpc_supTicket.SrvImpl)
 
-/*
+
 server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
     console.log("Server started")
     server.start(); })
@@ -25,4 +25,4 @@ server2.bindAsync('0.0.0.0:3030', ServerCredentials.createInsecure(), () => {
     console.log("Server2 started")
     server2.start(); })
 
-*/
+
