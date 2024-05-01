ed3327e59ca1dbf174b0cc1be723f92069efe1f9
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-01 22:22:50 +0300

    Update grpc-reflect.ts

--- src/grpc-reflect.ts
+++ src/grpc-reflect.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import {server,server2} from "./grpc";
-import { loadPackageDefinition }  from "@grpc/grpc-js";
+import { loadPackageDefinition, ServerCredentials  }  from "@grpc/grpc-js";
 import { ReflectionService } from '@grpc/reflection';
 const protoLoader = require('@grpc/proto-loader');
 //console.log(protoLoader,ReflectionService)
