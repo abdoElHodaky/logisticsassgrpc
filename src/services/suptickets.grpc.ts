9be1069a505340bd1001dd30c4d544a47ed5f79e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-02 16:32:00 +0300

    Update suptickets.grpc.ts

--- src/services/suptickets.grpc.ts
+++ src/services/suptickets.grpc.ts
@@ -7,7 +7,7 @@ import { Service } from "../service.decorator";
 export class supTicketGrpcService  {
   
  @Service("Ticket")
-  static service:any
+  static service:any=new supTicketService()
   //public [name: string]:UntypedHandleCall;
   public SrvImpl: _Ticket.TicketServiceServer = {
    async all (call: ServerUnaryCall<_Ticket.GetAllTicketReq,_Ticket.GetAllTicketRes>,
