27c0820eea3883f29f7b907736a572da52357e4a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-03 14:42:23 +0300

    Update suptickets.grpc.ts

--- src/services/suptickets.grpc.ts
+++ src/services/suptickets.grpc.ts
@@ -21,7 +21,9 @@ export class supTicketGrpcService  {
      let {user,created_at,updated_at}=tickets[inx]
      if(user!=null) {
       a.userId=user.id
-      a.created_at=created_at.getTime()
+      a.created_at=created_at
+      a.updated_at=updated_at
+      console.log(a.created_at instanceof Date)
      }
    //  else a.userId =Math.random(1,inx/3)
      })
