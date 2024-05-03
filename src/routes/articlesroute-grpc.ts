b2bcd05c73af35e0fe555decb2cd377953e249fe
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-03 12:53:32 +0300

    Update articlesroute-grpc.ts

--- src/routes/articlesroute-grpc.ts
+++ src/routes/articlesroute-grpc.ts
@@ -5,7 +5,7 @@ import { Router } from "express";
 
 export const articlesgrpc=Router()
 
-articlesgrpc.get("/grpc/articles",(req,res)=>{
+articlesgrpc.get("/articles",(req,res)=>{
     /* 	#swagger.tags = ['GRPC.Article']
         #swagger.description = 'Endpoint to get articles via grpc' */
 
