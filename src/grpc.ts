2af4cf50366885d117979288537a484825371e07
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-04 17:28:46 +0300

    Update grpc.ts

--- src/grpc.ts
+++ src/grpc.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
 import { Server, ServerCredentials, loadPackageDefinition }  from "@grpc/grpc-js";
 import { _Article,_Ticket,_Auth ,_User} from "./protos/dist/";
-import  * as protoLoader  from "@grpc/proto-loader";
-import { ReflectionService } from '@grpc/reflection';
+//mport  * as protoLoader  from "@grpc/proto-loader";
+//import { ReflectionService } from '@grpc/reflection';
 import { services } from "./services/enum";
 
 export const server = new Server()
@@ -16,7 +16,7 @@ server2.addService(_Ticket.TicketServiceService,services.Grpc_supTicket.SrvImpl)
 server2.addService(_Auth.AuthServiceService,services.Grpc_Auth.SrvImpl)
 server2.addService(_Article.ArticleServiceService,services.Grpc_Article.SrvImpl)
 
-protoLoader.load("./src/protos/src/index.proto").then((pkg:any)=>{
+/*protoLoader.load("./src/protos/src/index.proto").then((pkg:any)=>{
   pkg=loadPackageDefinition(pkg)
   const reflect=new ReflectionService(pkg)
   reflect.addToServer(server)
@@ -37,5 +37,5 @@ server2.bindAsync('0.0.0.0:3030', ServerCredentials.createInsecure(), () => {
     console.log("Server2 started")
     server2.start(); })
 
-
+*/
 
