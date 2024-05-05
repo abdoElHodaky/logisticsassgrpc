7e02063a55dd6f1aa5391e3a644e43392355d249
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-05 14:34:08 +0300

    Delete dist directory

--- dist/dto/create-payment.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreatePaymemtDto = void 0;
-class CreatePaymemtDto {
-}
-exports.CreatePaymemtDto = CreatePaymemtDto;
--- dist/includes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._datasource = exports.AppDataSource = exports.DataSource = void 0;
-var typeorm_1 = require("typeorm");
-Object.defineProperty(exports, "DataSource", { enumerable: true, get: function () { return typeorm_1.DataSource; } });
-var _datasource_1 = require("./_datasource");
-Object.defineProperty(exports, "AppDataSource", { enumerable: true, get: function () { return _datasource_1.AppDataSource; } });
-Object.defineProperty(exports, "_datasource", { enumerable: true, get: function () { return _datasource_1._datasource; } });
