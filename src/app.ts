b2c27481f0df8af60bd96b17522c12cee3a83aac
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-05 22:12:37 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,24 @@ const port = process.env.PORT||3000
 app.use(urlencoded({extended: true}))
 app.use(cors())
 app.use(json())
+app.use(
+  jwt({
+    secret: "secert",
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+    getToken: function fromHeaderOrQuerystring(req) {
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === "Bearer"
+      ) {
+        return req.headers.authorization.split(" ")[1];
+      } else if (req.query && req.query.token) {
+        return req.query.token;
+      }
+      return null;
+    },
+  }).unless({path:["/payments/callback","/payments/return"]})
+
 app.use(apiv1)
 /*app.use(rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
