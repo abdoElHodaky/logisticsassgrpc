366cfc065e6ce212300b2c335ed52abdbb1eafb9
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-05 14:42:32 +0300

    Update paymentroute.ts

--- src/routes/paymentroute.ts
+++ src/routes/paymentroute.ts
@@ -1,1 +1,21 @@
 
+import { Router } from "express";
+import { Request,Response } from "express";
+export const payroute=Router();
+payroute.get("/payments",function(req:Request, res:Response){
+    /* 	#swagger.tags = ['Payment']
+        #swagger.description = 'Endpoint to get   PAYMENTS' */
+
+    
+  })
+  
+  payroute.get("/payments/:paymentId/pay",function(req:Request, res:Response){
+   /* 	#swagger.tags = ['Payment']
+        #swagger.description = 'Endpoint to iniliatize specific payment'
+        #swagger.parameters["paymentId"]={
+         in:"path",
+         description:"Payment identification"
+        }
+     */
+   
+  })
