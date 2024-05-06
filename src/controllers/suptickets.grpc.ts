206df3ba37a52d59f9ef22e74960ed01c58675b4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 21:25:29 +0300

    Update suptickets.grpc.ts

--- src/controllers/suptickets.grpc.ts
+++ src/controllers/suptickets.grpc.ts
@@ -2,6 +2,7 @@ import { credentials } from "@grpc/grpc-js";
 import {_Ticket } from "../protos/dist/";
 import { Res,  Controller , Get } from "@decorators/express";
 import { Response  } from "express";
+import { Request } from "express-jwt"
 
 const address = "localhost:3030";
 
