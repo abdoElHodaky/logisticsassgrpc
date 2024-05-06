9644b87896e3e699efabae10459393fb3c927707
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 17:47:55 +0300

    Update auth.grpc.ts

--- src/controllers/auth.grpc.ts
+++ src/controllers/auth.grpc.ts
@@ -26,9 +26,9 @@ export class GrpcAuthController {
     } else {
        // let _user=new User()
         //let usee=_User.User.toJSON(resp.user)
-        let {user}=resp
+        let user=resp.user
         console.log(user)
-       let token =jwt.sign({userId:user.id},"secret", { expiresIn: 60 * 60 })
+       let token =jwt.sign({userId:user["id"]},"secret", { expiresIn: 60 * 60 })
        res.json({accessToken:token})
      }
     })
