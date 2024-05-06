d088724597a16fec4e3758a301eb6adb767a7fcc
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 21:14:42 +0300

    Update authenticate.middleware.ts

--- src/authenticate.middleware.ts
+++ src/authenticate.middleware.ts
@@ -1,17 +1,17 @@
 import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
-//import { Request as } from "express-jwt";
+import { Request as JWTRequest } from "express-jwt";
 import {services} from "./services/enum";
 var jwt = require('jsonwebtoken');
 export class AuthenticateMiddleware implements Middleware {
-  async use(request: Request, res: Response, next: NextFunction): Promise<void> {
+  async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const req= request
     const service=services.User
    // let user= await service.id(req?.auth.id)
     let authorizeHeader=req.headers?.authorization
     if(authorizeHeader!==undefined){ 
       let token=authorizeHeader.split(" ")[1]
-      req["user"]=await service.id(jwt.verify(token,"secret").id)
+      req.auth= await service.id(jwt.verify(token,"secret").id)
     }
    // res.end("")
     next();
