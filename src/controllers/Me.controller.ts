10a775681d8751d30e43fda0acf171ec52ed4d85
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 21:40:45 +0300

    Update Me.controller.ts

--- src/controllers/Me.controller.ts
+++ src/controllers/Me.controller.ts
@@ -1,1 +1,20 @@
+import { credentials } from "@grpc/grpc-js";
+import {_User } from "../protos/dist/";
+import { Res,  Controller , Get ,Post,Req} from "@decorators/express";
+import { Response } from "express";
+import { Request } from "express-jwt";
+import {AuthenticateMiddleware} from "../authenticate.middleware";
+const address = "localhost:50051";
+@Controller("/me")
+export class GrpcMeController {
+  private client =new _User.UserServiceClient(
+    address,
+    credentials.createInsecure()
+  )
+  
+  @Get("",[AuthenticateMiddleware])
+  async all(@Req() req:Request,@Res() res:Response ):Promise<void>{
+    res.json(req.auth)
+  }
 
+}
