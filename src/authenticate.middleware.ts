34ca7998b5a91c708f663f3d7cbb5c7d02ddafb4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 12:10:08 +0300

    Update authenticate.middleware.ts

--- src/authenticate.middleware.ts
+++ src/authenticate.middleware.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@decorators/express';
-import {Request,Response,NextFunction} from "express";
+import {Request,Response,NextFunction, MiddlewareFunction  } from "express";
 export class AuthenticateMiddleware implements Middleware {
-  public use(request: Request, response: Response, next: NextFunction): void {
+  public use(req: Request, res: Response, next: NextFunction): void {
    if((req.auth && (req.auth=={}|| req.auth==null )) || (req.user && (req.user=={} || req.user==null))){
      res.status(401).json({message:"Please sign in"})
    }
