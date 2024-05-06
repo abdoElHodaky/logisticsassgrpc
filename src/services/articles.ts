ee833e79c75b5bfca833459cd2a9b74f29fb9c48
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 23:37:44 +0300

    Update articles.ts

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -27,7 +27,7 @@ export class ArticleService extends _Data {
     _article.title=article.title
    _article.imgurl=article.imgurl
    _article.content=article.content
-   _article.cateogry=article.cateogry
+  // _article.cateogry=article.cateogry
    /* let author=await this.datasource.manager.findOneByOrFail(Author,{id:userId})
     _article.author=author
     author.articles.push(_article)
