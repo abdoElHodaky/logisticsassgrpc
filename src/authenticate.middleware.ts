89ceb5ce11876af6691151965dd1e60ea8046bc6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 17:03:25 +0300

    Update authenticate.middleware.ts

--- src/authenticate.middleware.ts
+++ src/authenticate.middleware.ts
@@ -3,7 +3,7 @@ import {Request,Response,NextFunction  } from "express";
 import { expressjwt, ExpressJwtRequest } from "express-jwt";
 import {services} from "./services/enum";
 export class AuthenticateMiddleware implements Middleware {
-  async use(request: Request, res: Response, next: NextFunction): void {
+  async use(request: Request, res: Response, next: NextFunction): Promise<void> {
     const req= request as ExpressJwtRequest
     const service=services.User
     let user= await service.id(req?.auth.id)
