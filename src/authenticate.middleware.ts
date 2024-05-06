1f120455d31e0b5dd19a5716512378dad788f01a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 13:09:58 +0300

    Update authenticate.middleware.ts

--- src/authenticate.middleware.ts
+++ src/authenticate.middleware.ts
@@ -2,7 +2,7 @@ import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 export class AuthenticateMiddleware implements Middleware {
   public use(req: Request, res: Response, next: NextFunction): void {
-    console.log(req)
+    console.log(req?.auth)
     next();
   }
 }
