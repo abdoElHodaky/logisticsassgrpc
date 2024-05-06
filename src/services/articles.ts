f5d57c209b96a7a5a27e6ecede639b7ff1ca3ed1
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 22:39:12 +0300

    Update articles.ts

--- src/services/articles.ts
+++ src/services/articles.ts
@@ -21,9 +21,9 @@ export class ArticleService extends _Data {
 
  async create(createArticleDto:CreateArticleDto):Promise<Article|void>{
      
-    const {userId,...article}=createArticleDto
-    let _article=this.datasource.manager.create(Article,article)
-    let author=await this.datasource.manager.findOneByOrFail(Author,{id:parseInt(userid)})
+    const {userId,...res}=createArticleDto
+    let _article=this.datasource.manager.create(Article,res)
+    let author=await this.datasource.manager.findOneByOrFail(Author,{id:parseInt(userId)})
     _article.author=author
     author.articles.push(_article)
     _article=await this.datasource.manager.save(Article,_article)
