bacac589d06d19ccbd8250c460f52ae750c5a058
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 13:42:42 +0300

    Update authenticate.middleware.ts

--- src/authenticate.middleware.ts
+++ src/authenticate.middleware.ts
@@ -2,7 +2,8 @@ import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { expressjwt, ExpressJwtRequest } from "express-jwt";
 export class AuthenticateMiddleware implements Middleware {
-  public use(req: Request, res: Response, next: NextFunction): void {
+  public use(request: Request, res: Response, next: NextFunction): void {
+    const req= request as ExpressJwtRequest
     console.log(req?.auth)
     next();
   }
