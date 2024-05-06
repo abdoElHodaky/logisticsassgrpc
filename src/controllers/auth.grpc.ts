84d4e8983b5b9de8f79fa07c43f2369dd648c576
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-06 19:33:17 +0300

    Update auth.grpc.ts

--- src/controllers/auth.grpc.ts
+++ src/controllers/auth.grpc.ts
@@ -29,7 +29,7 @@ export class GrpcAuthController {
         let user=resp.user
         if(user!=undefined){
         console.log(user)
-       let token =jwt.sign({data: user}, 'secret', { expiresIn: '1h' , algorithms: ["HS256"] });
+       let token =jwt.sign(user, 'secret', { expiresIn: '1h' , algorithms: ["HS256"] });
        res.json({accessToken:token})
      }
         else {
