3aec2824d13fd08a2f4e2f48f556a9ed37e1ee8a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-07 15:14:22 +0300

    Update author.ts

--- src/middlewares/author.ts
+++ src/middlewares/author.ts
@@ -7,8 +7,13 @@ export class AuthorMiddleware implements Middleware {
   async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const req= request
     const service=services.User
+    if(req.auth !=undefined){
     let user= await service.id(req?.auth.id)
     if(user.type!="Author") res.jsonp({message:"Logged in User not Author"})
+    }
+    else {
+      res.status(401).json({message:"you should login"})
+    }
     next();
   }
 }
