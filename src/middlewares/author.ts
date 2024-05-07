889ff364b9536520e34944ba2d00751a794712ce
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-07 14:46:50 +0300

    Update author.ts

--- src/middlewares/author.ts
+++ src/middlewares/author.ts
@@ -1,1 +1,13 @@
-
+import { Middleware } from '@decorators/express';
+import {Request,Response,NextFunction  } from "express";
+import { Request as JWTRequest } from "express-jwt";
+import {services} from "../services/enum";
+var jwt = require('jsonwebtoken');
+export class AuthorMiddleware implements Middleware {
+  async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
+    const {auth}= request
+    
+   // res.end("")
+    next();
+  }
+}
