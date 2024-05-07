85480adb2dd96f3f81a3f8073c933425694ceff2
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-07 15:06:32 +0300

    Update author.ts

--- src/middlewares/author.ts
+++ src/middlewares/author.ts
@@ -2,12 +2,13 @@ import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
 import {services} from "../services/enum";
-var jwt = require('jsonwebtoken');
+//var jwt = require('jsonwebtoken');
 export class AuthorMiddleware implements Middleware {
   async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
-    const {auth}= request
-    
-   // res.end("")
+    const req= request
+    const service=services.User
+    let user= await service.id(req?.auth.id)
+    if(user.type!="Author") res.jsonp({message:"Logged in User not Author"})
     next();
   }
 }
