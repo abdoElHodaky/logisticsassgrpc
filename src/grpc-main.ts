c6ff0d1da2bef3d15d730a75f5864f727962ac5b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-08 09:06:23 +0300

    Update grpc-main.ts

--- src/grpc-main.ts
+++ src/grpc-main.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 import {server,server2} from "./grpc";
 import { loadPackageDefinition, ServerCredentials  }  from "@grpc/grpc-js";
 import { ReflectionService } from '@grpc/reflection';
+import {Env} from "../env";
+const {GRP_CPORT,GRPCPORT}=Env
 const protoLoader = require('@grpc/proto-loader');
 protoLoader.load("./src/protos/src/index.proto").then((pkg:any)=>{
   pkg=loadPackageDefinition(pkg)
@@ -16,11 +18,11 @@ protoLoader.load("./src/protos/src/index.proto").then((pkg:any)=>{
   
 }).catch(console.log)
 
-server.bindAsync('0.0.0.0:50051', ServerCredentials.createInsecure(), () => {
+server.bindAsync(`0.0.0.0:${GRP_CPORT}`, ServerCredentials.createInsecure(), () => {
     console.log("Server started")
     server.start(); })
 
-server2.bindAsync('0.0.0.0:3030', ServerCredentials.createInsecure(), () => {
+server2.bindAsync(`0.0.0.0:${GRPCPORT}`, ServerCredentials.createInsecure(), () => {
     console.log("Server2 started")
     server2.start(); })
 
