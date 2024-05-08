2795ff2f1dda76f4ea8ca05c2725c7cd1256d689
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-08 13:56:51 +0300

    Update auth.grpc.ts

--- src/controllers/auth.grpc.ts
+++ src/controllers/auth.grpc.ts
@@ -5,6 +5,7 @@ import { Res,  Controller , Post ,Body } from "@decorators/express";
 import { Response  } from "express";
 import { LoginUserDto } from "../dto/";
 import { Error } from "common-errors";
+import { isEmpty} from "../helpers";
 import {Env} from "../env";
 const address = "localhost:"+Env.GRP_CPORT
 var jwt = require('jsonwebtoken');
@@ -17,7 +18,7 @@ export class GrpcAuthController {
   
   @Post("/login")
   async login(@Res() res:Response, @Body() loginUserDto:LoginUserDto ):Promise<void>{
-   if(Object.values(loginUserDto).some(o=> o=="" || o==null)==false){
+   if(isEmpty(loginUserDto)==false){
     const req:_Auth.LoginUserReq={
       username:loginUserDto.username,
       passwordHash:loginUserDto.passwordHash
@@ -48,7 +49,8 @@ export class GrpcAuthController {
     }
   } 
   else{
-    throw new Error()
+    throw new Error("Login Information not provided or not existed")
+   
   }
 }
  
