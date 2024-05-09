4b6fac4dfa41899d5d8cb164329addc99cfd53e7
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-09 18:48:09 +0300

    Update userequality.ts

--- src/middlewares/userequality.ts
+++ src/middlewares/userequality.ts
@@ -4,7 +4,7 @@ import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
 import {services} from "../services/enum";
 //var jwt = require('jsonwebtoken');
-export class UserEquityMiddleware implements Middleware {
+export class UserEqulityMiddleware implements Middleware {
   async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const req= request
    // const service=services.User
