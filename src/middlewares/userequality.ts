f42c79e15e40b9757d496fe5e0e91f2d2f51f34e
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-09 18:47:40 +0300

    Update userequality.ts

--- src/middlewares/userequality.ts
+++ src/middlewares/userequality.ts
@@ -1,1 +1,20 @@
 
+import { Middleware } from '@decorators/express';
+import {Request,Response,NextFunction  } from "express";
+import { Request as JWTRequest } from "express-jwt";
+import {services} from "../services/enum";
+//var jwt = require('jsonwebtoken');
+export class UserEquityMiddleware implements Middleware {
+  async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
+    const req= request
+   // const service=services.User
+    const {auth,params}=req
+    if(auth !=undefined && params.includes("userId")){
+      if (auth?.id!=parseInt(params["userId"]  )){
+       res.status(403).jsonp({message:"userId should equal your Id"})
+      }   
+    }
+      
+    next();
+  }
+}
