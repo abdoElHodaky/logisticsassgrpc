822bd7eea0262689ba07387e383b8a5957e66a6c
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-09 09:29:07 +0300

    Update tickets.grpc.ts

--- src/controllers/tickets.grpc.ts
+++ src/controllers/tickets.grpc.ts
@@ -2,11 +2,11 @@ import { credentials } from "@grpc/grpc-js";
 import {_Ticket } from "../protos/dist/";
 import { Res,  Controller , Get ,Params } from "@decorators/express";
 import { Response  } from "express";
-
+import { AuthenticateMiddleware} from "../middlewares/";
 import {Env} from "../env";
 const address = "localhost:"+Env.GRP_CPORT
 
-@Controller("/users")
+@Controller("/users",[AuthenticateMiddleware])
 export class GrpcTicketController {
   private client =new _Ticket.TicketServiceClient(
     address,
