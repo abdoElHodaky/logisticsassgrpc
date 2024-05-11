f03cc48e893692cab9b14ed4d43b656c47575cd5
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-11 23:28:22 +0300

    Create Purshase.ts

---
+++ src/entity/Purshase.ts
@@ -0,0 +1,14 @@
+import { Entity ,Column,PrimaryGeneratedColumn, ManyToOne,OneToOne ,ChildEntity} from "typeorm"
+import { User} from "./";
+import { Payment } from "./";
+
+@Entity()
+//@ChildEntity()
+export class Purshase {
+    
+   
+
+
+    @ManyToOne(()=>User,purchase=>purchase.user) user:User;
+
+}
