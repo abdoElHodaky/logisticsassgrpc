126cae06cf23a7cffa570928d9e9ee84e2aa0b0a
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-11 08:32:28 +0300

    Update users.ts

--- src/services/users.ts
+++ src/services/users.ts
@@ -46,5 +46,28 @@ export class UserService extends _Data {
     }
     else return new TypeError("userId should be number")
   }
+  async defaults():Promise<void>{
+    let users=  await this.datasource.userRepository.find({
+  where: [
+    {
+      username: '',
+      passwordHash: '',
+    },
+    {
+      username: null,
+      passwordHash:null,
+    },
+  ],
+});
+ users=users.map(e=>{
+  e.username= `test_279346__${e.id}`
+  e.passwordHash=`test_297438__${id}`
+  e.firstName='Timber'
+  e.lastName='Saw'
+ })
+    
+await this.datasource.manager.save(User,users)
+    
+  }
   
 }
