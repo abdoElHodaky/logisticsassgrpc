e07c953f284c4dc965ec206a04fea3b059415425
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-11 22:52:56 +0300

    Update me.controller.ts

--- src/controllers/me.controller.ts
+++ src/controllers/me.controller.ts
@@ -4,14 +4,14 @@ import { Res,  Controller , Get ,Post,Req} from "@decorators/express";
 import { Response } from "express";
 import { Request } from "express-jwt";
 import {AuthenticateMiddleware} from "../middlewares/authenticate";
-const address = "localhost:50051";
+//const address = "localhost:50051";
 @Controller("/me")
 export class GrpcMeController {
-  private client =new _User.UserServiceClient(
+ /* private client =new _User.UserServiceClient(
     address,
     credentials.createInsecure()
   )
-  
+  */
   @Get("",[AuthenticateMiddleware])
   async me(@Req() req:Request,@Res() res:Response ):Promise<void>{
     res.json(req.auth)
