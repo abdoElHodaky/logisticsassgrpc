79073a198941a93eca230f53b52c8574e5c09e69
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-12 11:37:52 +0300

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,3 @@
-/*import {AuthController} from "./auths";
-import {AuthorController} from "./authors";
-import {UserController} from "./users";
-import {UserTicketController} from "./users.tickets";
-import {ArticleController} from "./articles";
-import {AttachmentController} from "./attachments";*/
 import {GrpcArticleController} from "./articles.grpc";
 import {GrpcUserTicketController} from "./users-tickets.grpc";
 import {GrpcAuthController} from "./auth.grpc";
@@ -12,7 +6,8 @@ import {GrpcUserController} from "./users.grpc";
 import {GrpcSupTicketController} from "./suptickets.grpc";
 import {PaymentController} from "./payments";
 import {GrpcMeController} from "./me";
-import {GrpcUserPaymentController} from "users-payments.grpc";
+import {GrpcUserPaymentController} from "./users-payments.grpc";
+
 export const controllers=[GrpcAuthController,GrpcAuthorController,
                           GrpcArticleController,GrpcTicketController,
                           GrpcUserController,GrpcSupTicketController,
