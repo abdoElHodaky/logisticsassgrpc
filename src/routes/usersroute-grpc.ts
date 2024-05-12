a614e5270487c793faec7079dd920f9e10ad7766
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-12 14:34:27 +0300

    Update usersroute-grpc.ts

--- src/routes/usersroute-grpc.ts
+++ src/routes/usersroute-grpc.ts
@@ -5,7 +5,7 @@ import { Router } from "express";
 
 export const usersgrpc=Router()
 export const megrpc=Router()
-
+export const userspayrpc=Router()
 usersgrpc.get("/users",(req,res)=>{
     /* 	#swagger.tags = ['GRPC.User']
         #swagger.description = 'Endpoint to get authors via grpc' */
@@ -22,4 +22,19 @@ megrpc.get("/me",(req,res)=>{
     
     */
 
+})
+
+userspayrpc.get("/users/:userId/tickets",(req,res)=>{
+    /* 	#swagger.tags = ['GRPC.User.Ticket']
+        #swagger.description = 'Endpoint to get articles via grpc' 
+        #swagger.parameters['userId'] = {
+            in: 'path',
+            description: 'id of specific user.',
+            
+    }
+    #swagger.security = [{
+            "JWTAuth": []
+       }]
+
+    */
 })
