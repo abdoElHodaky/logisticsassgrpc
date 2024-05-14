2a6655337a5dd9494faf2695a03e349e9efc0cbf
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-14 13:36:57 +0300

    Update Book.ts

--- src/entity/Book.ts
+++ src/entity/Book.ts
@@ -1,8 +1,8 @@
-import { ChildEntity, Column, Entity } from "typeorm"
+import { ChildEntity, Column, Entity ,BaseEntity} from "typeorm"
 import { Attachment } from "./Attachment";
 
 @ChildEntity()
-export class Book extends Attachment<T> {
+export class Book extends Attachment<T extends BaseEntity> {
     @Column({default:"book"})
     type:string
 
