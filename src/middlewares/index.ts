dccb3e6099c603f0bf1c08b9b08e2c48fc5c2348
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-14 09:56:56 +0300

    Update index.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export {AuthenticateMiddleware} from "./authenticate";
 export {AuthorMiddleware} from "./author";
 export {UserEqulityMiddleware} from "./userequality";
+export {ValidatedMiddleware} from "./validated";
