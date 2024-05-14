fdf63c041c814200962a1b74cacec1844770dfa6
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-14 18:42:32 +0300

    Update Purshase.ts

--- src/entity/Purshase.ts
+++ src/entity/Purshase.ts
@@ -1,14 +1,14 @@
 import { Entity ,Column,PrimaryGeneratedColumn,OneToMany ,OneToOne,ManyToOne ,ChildEntity} from "typeorm"
 import { User} from "./";
 import { Payment } from "./";
-
+import { Attachment	} from "./";
 @Entity()
 //@ChildEntity()
 export class Purshase {
     @PrimaryGeneratedColumn("increment")
     id: number;
    
- 
+    
     @OneToMany(()=>PurshaseItem, item=>item.purshase) items:PurshaseItem[]
     //@ManyToOne(()=>User,user=>user.purchases) user:User;
     @OneToOne(()=>Payment,payment=>payment.purchase) payment:Payment
@@ -22,6 +22,8 @@ export class PurshaseItem {
     
     @Column("simple-json")
     props:any
-
+    
+    @OneToMany(()=>Attachment, attachment=>attachment.entity) attachments:Attachment[]
+    
     @ManyToOne(()=>Purshase,purshase=>purshase.items) purshase:Purshase;
 }
