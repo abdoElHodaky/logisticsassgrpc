011188895e7652de8de5c41b4f55b443c0b3be17
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-14 08:43:45 +0300

    Update validated.ts

--- src/middlewares/validated.ts
+++ src/middlewares/validated.ts
@@ -1,15 +1,13 @@
-
+import { validate } from 'class-validator';
 import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
-//import {services} from "../services/enum";
-//var jwt = require('jsonwebtoken');
 export class ValidatedMiddleware implements Middleware {
   async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const req= request
-  
-     
-    }
+    const errors=await validate(req?.body)
+    if(errors) res.status(400).jsonp(errors)
+    
       
     next();
   }
