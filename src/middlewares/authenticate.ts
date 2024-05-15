df317c5241571db11dc84ec59a3c00cbbeac5f00
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-15 12:37:32 +0300

    Update authenticate.ts

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -2,18 +2,19 @@ import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
 import {services} from "../services/enum";
+import { Env } from "../env";
 var jwt = require('jsonwebtoken');
 export class AuthenticateMiddleware implements Middleware {
   async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const req= request
     const service=services.User
-   // let user= await service.id(req?.auth.id)
+    const secret=Env.JWT_SECRET || "secret"
     let authorizeHeader=req.headers?.authorization
     if(authorizeHeader!==undefined){ 
       let token=authorizeHeader.split(" ")[1]
-      let decoded=jwt.decode(token,"secret")
+      let decoded=jwt.decode(token,secret)
       if(Date.now() < decoded.exp * 1000) {
-        decoded=jwt.verify(token,"secret")
+        decoded=jwt.verify(token,secret)
         req.auth= await service.id(decoded.id)
       }
       else{res.status(400).json({message:"Token expired, you should login"})}
