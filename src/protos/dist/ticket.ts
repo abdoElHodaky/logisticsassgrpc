de4445ecfbc9f8fd827adfd6d8f79c04cfa05161
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-15 12:25:08 +0300

    Update ticket.ts

--- src/protos/dist/ticket.ts
+++ src/protos/dist/ticket.ts
@@ -177,10 +177,10 @@ export const Ticket = {
       obj.userId = Math.round(message.userId);
     }
     if (message.createdAt !== undefined) {
-      obj.createdAt = message.createdAt.toISOString();
+      obj.createdAt = message.createdAt.toLocaleString("en-EG");
     }
     if (message.updatedAt !== undefined) {
-      obj.updatedAt = message.updatedAt.toISOString();
+      obj.updatedAt = message.updatedAt.toLocaleString("en-EG");
     }
     return obj;
   },
