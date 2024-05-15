9599261b32d1c0fc47283625261a336520cb6651
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-15 11:40:24 +0300

    Update User.ts

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -11,10 +11,10 @@ export class User {
     id: number
 
     @Column()
-    firstName: string
+    firstname: string
 
     @Column()
-    lastName: string
+    lastname: string
 
     @Column()
     age: number
