a350f5983f94fd626cffaf09c258a90cf5c77cac
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-16 12:02:58 +0300

    Update login-user.dto.ts

--- src/dto/login-user.dto.ts
+++ src/dto/login-user.dto.ts
@@ -1,5 +1,8 @@
+import { IsNotEmpty} from "class-validator";
 export class LoginUserDto {
+  @IsNotEmpty()
   readonly username:string
   readonly email:string
+  @IsNotEmpty()
   readonly passwordHash:string
 }
