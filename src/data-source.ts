893cf649cc1d8e7b5615aa70780809208ab960db
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-16 22:13:42 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { InjectionToken } from "@decorators/di"
-import { Article,Attachment,User,Author,Book,supTicket,Verification,Payment,Purshase,PurshaseItem } from "./entity/";
+import { Article,Attachment,User,Author,Book,supTicket,Verification,Payment,Purshase,PurshaseItem ,Product} from "./entity/";
 
 export const _AppDataSource = new DataSource({
     type: "sqlite",
@@ -14,7 +14,7 @@ export const _AppDataSource = new DataSource({
     entities: [User,supTicket,Article,
                Verification,Author,Book,
                Payment,Purshase,PurshaseItem,
-               Attachment
+               Attachment,Product
               ],
     migrations: [],
     subscribers: [],
