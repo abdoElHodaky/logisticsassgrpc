f2914ac05c18f881dc7653e9300bc710aaf09780
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-16 21:54:04 +0300

    Update attachments.ts

--- src/services/attachments.ts
+++ src/services/attachments.ts
@@ -1,6 +1,7 @@
 import { Injectable , Inject } from "@decorators/di";
 import { DataSource ,AppDataSource ,_datasource } from "../includes"
 import { Attachment } from "../entity/"
+import { AttachedEnity } from "../entity/Attachment";
 //import { CreateArticleDto } from "../dto/create-article.dto"
 
 //@Injectable()
@@ -8,10 +9,14 @@ export class AttachmentService {
     private datasource:DataSource=AppDataSource
   constructor (){}
 
-  async all():Promise<Attachment[]>
+  async all(type?:AttachedEnity):Promise<Attachment[]>
   {
     //console.log(this._source)
-    return await this.datasource.manager.find(Attachment)
+    return await this.datasource.manager.find(Attachment,{
+        where:{
+            forType:type
+        }
+    })
   }
 
 /* async create(createArticleDto:CreateArticleDto):Promise<Article|void>{
