3df25d0c2ee0158b0f4655591a42211125ce33c8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-16 14:54:35 +0300

    Update Attachment.ts

--- src/entity/Attachment.ts
+++ src/entity/Attachment.ts
@@ -26,5 +26,5 @@ export class Attachment {
     source:string;
 
     @ManyToOne(()=>User,uploader=>uploader.media) uploader:User;
-    @ManyToOne(attachedTo=>attachedTo?.attachments) attachedTo:T;
+    @ManyToOne(attachedTo=>attachedTo?.attachments) attachedTo:PurshaseItem|Article;
 }
