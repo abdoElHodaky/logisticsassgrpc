d959cc4b8a333ea94a385cbf8f61c5091c92e6b0
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-16 21:45:21 +0300

    Update Product.ts

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,8 +1,15 @@
 import { Entity ,Column,PrimaryGeneratedColumn, OneToMany, ChildEntity} from "typeorm"
-import { User} from "./";
+import { User,Attachment} from "./";
 
 @Entity()
 //@ChildEntity()
 export class Product {
+    @PrimaryGeneratedColumn("increment")
+    id:number 
+
+    @Column("simple-json")
+    specs:any
+
+    attachments:Attachment[]
     
 }
