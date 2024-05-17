207eba82b13aa66f8841a61d92878bcf7c4163b4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-17 11:46:26 +0300

    Update Product.ts

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,6 +1,6 @@
 import { Entity ,Column,PrimaryGeneratedColumn, OneToMany, ChildEntity} from "typeorm"
 import { User,Attachment} from "./";
-import { AttachedType } from "./Attachment";
+import { AttachedType } from "./Attached";
 @Entity()
 //@ChildEntity()
 export class Product {
