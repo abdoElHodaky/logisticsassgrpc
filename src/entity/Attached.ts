a1065623b2511136dad24ec1a61727d4c62fb7df
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-17 21:35:02 +0300

    Update Attached.ts

--- src/entity/Attached.ts
+++ src/entity/Attached.ts
@@ -1,13 +1,10 @@
-import { ChildEntity} from "typeorm";
+import { ChildEntity,ManyToOne,Column} from "typeorm";
 import { Attachment} from "./Attachment";
 import { Product} from "./Product";
 @ChildEntity()
 export class  ProductAttachment extends Attachment {
-    
+    @Column({default:Product.name})
+    forType:string
     @ManyToOne(()=>Product,attached=>attached.attachments) attached:Product;
    
 } 
-export enum AttachedEnity{
-    ARTICLE="Article",
-    ITEM="PurshaseItem"
-}
