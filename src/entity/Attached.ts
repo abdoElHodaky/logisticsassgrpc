9c52a5b658f26bf4ece52d3e028fe099a800e50d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-17 12:37:53 +0300

    Update Attached.ts

--- src/entity/Attached.ts
+++ src/entity/Attached.ts
@@ -1,8 +1,8 @@
 import { BaseEntity} from "typeorm";
 import { Attachment} from "./Attachment";
 
-export class  TypeAttachment extends {attachments:Attachment[]} {}
-export class  AttachedType extends {attached:TypeAttachment} {}
+export class  TypeAttachment  {attachments:Attachment[]} 
+export class  AttachedType  {attached:TypeAttachment} 
 export enum AttachedEnity{
     ARTICLE="Article",
     ITEM="PurshaseItem"
