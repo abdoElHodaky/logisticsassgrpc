5453a0d8b8b212fa4e2aaae0e59ddabb1198ce0d
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-19 18:52:15 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -1,12 +1,8 @@
 #!/user/bin/bash
 
-if [[ "$ENABLE_OVERCOMMIT_MEMORY" == 'true' ]]; then
-	echo "vm.overcommit_memory=1" | sudo tee -a /etc/sysctl.conf
-        sudo sysctl -p
-fi
+node run build
 node swagautogen.js
 
 #node ./dist/grpc-reflect.js
-#redis-server --loglevel warning --bind 0.0.0.0   && 
 node ./dist/app.js 
 #redis-server
