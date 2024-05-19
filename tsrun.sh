029ff9af62ca8e3650d9853f9a2fac631fbb9dfb
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-19 12:59:45 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -7,5 +7,6 @@ fi
 node swagautogen.js
 
 #node ./dist/grpc-reflect.js
-redis-server --loglevel warning --bind 0.0.0.0   && node ./dist/app.js 
+#redis-server --loglevel warning --bind 0.0.0.0   && 
+node ./dist/app.js 
 #redis-server
