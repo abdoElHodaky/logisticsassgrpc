8c4b94f695318552a631f20f7c344fc76cbc7dc4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-21 23:36:38 +0300

    Update articles.grpc.ts

--- src/controllers/articles.grpc.ts
+++ src/controllers/articles.grpc.ts
@@ -26,7 +26,9 @@ export class GrpcArticleController {
       res.jsonp(err);
         console.error(err)
     } else {
-       res.json(_Article.GetAllRes.toJSON(resp))
+        const {articles}=_Article.GetAllRes.toJSON(resp)
+        console.log(articles)
+       res.json(resp)
      }
     })
   }
