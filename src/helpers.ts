59aa24ae4c379474ce39e0f046fe1de5a6b02dc4
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-23 10:36:17 +0300

    Update helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,7 @@
 export function isNumeric(value:string) {
     return /^\d+$/.test(value);
   }
+
 export function nationalIdvalid(value:string){
     const message=value
     const pattern=/^([1-9]{1})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})[0-9]{3}([0-9]{1})[0-9]{1}$/
@@ -9,4 +10,9 @@ export function nationalIdvalid(value:string){
 
 export function isEmpty(obj:any):boolean{
     return Object.values(obj).some(o=> o==null || o=="")
+}
+
+export function genVerfCode(){
+    const {randomInt}=require("crypto")
+    return randomInt(0, 1000000)toString().padStart(6, "0");
 }
