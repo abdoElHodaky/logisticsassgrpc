61292a6fbc3b577bbf8153bb8e132a5267bc0d2b
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-24 19:12:28 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,29 +1,31 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { InjectionToken } from "@decorators/di"
-import * as entities from "./entity/";
-const redisconn = require('redis-url').parse("rediss://red-cp4soqocmk4c73eom0p0:kLoGjFxqLJRRHFQs1QUaImdvOtnNdF19@oregon-redis.render.com:6379")
-console.log(Object.keys(entities))
+import { Article,Attachment,ProductAttachment,
+User,Author,supTicket,
+Verification ,Product,Purshase , PurshaseItem } from "./entity/"
 export const _AppDataSource = new DataSource({
     type: "sqlite",
-    database:"grpcendpoints.sqlite",
+    database: "grpcendpoints.sqlite",
     cache: {
-       // type: "ioredis",
-        //options: redisconn,
-        duration:300000
+        duration: 300000
     },
     synchronize: true,
     logging: false,
-    entities: [ ...(Object.keys(entities) ) ],
+    entities: [
+               Article,Attachment,ProductAttachment,
+               User,Author,supTicket,Purshase
+               Verification ,Product,PurshaseItem],
     migrations: [],
     subscribers: [],
     extra: {
+        connectionTimeoutMillis: Number.MAX_SAFE_INTEGER,
+        idleTimeoutMillis: Number.MAX_SAFE_INTEGER
     }
 })
 _AppDataSource.initialize()
   .then(async () => {
     console.log("Connection initialized with database...");
-	 // console.log(_AppDataSource)
   })
   .catch((error) => console.log(error));
 
@@ -38,18 +40,17 @@ export const getDataSource =  (delay = 3000): Promise<DataSource> => {
   });
 };
 
-/*var interval=setInterval(function (){
+setInterval(function (){
 if(_AppDataSource.isConnected==false)
 {
   _AppDataSource.connect().then(e=>{
-  console.log("connected")
-	  clearInterval(interval)
+  console.log("connected",e)
   }).catch(console.log)
 }
 else{
  console.log(" already connected")
 }
 
-},500000)*/
+},500000)
 
 export const _datasource=new InjectionToken("DataSource")
