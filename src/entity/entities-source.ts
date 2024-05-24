024ac354944b3c20f109f41d41ce1390bf9fb1ba
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-24 19:45:41 +0300

    Create entities-source.ts

---
+++ src/entity/entities-source.ts
@@ -0,0 +1,9 @@
+import {Article,Attachment,Product,ProductAttachment,
+  Author,User,Purshase,PurshaseItem,Payment,
+  supTicket,Verification} from "./";
+
+export const all=[
+  Article,Attachment,Product,ProductAttachment,
+  Author,User,Purshase,PurshaseItem,Payment,
+  supTicket,Verification
+]
