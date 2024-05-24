ddb0d0a8f6bbe24f98ea174420cda5ae1d4589c8
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-24 14:48:07 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 import { InjectionToken } from "@decorators/di"
 import * as entities from "./entity/";
 const redisconn = require('redis-url').parse("rediss://red-cp4soqocmk4c73eom0p0:kLoGjFxqLJRRHFQs1QUaImdvOtnNdF19@oregon-redis.render.com:6379")
-console.log([Object.keys(entities)])
+//console.log([Object.keys(entities)])
 export const _AppDataSource = new DataSource({
     type: "mysql",
     host:"db4free.net",
@@ -19,7 +19,7 @@ export const _AppDataSource = new DataSource({
     poolSize:3,
     synchronize: true,
     logging: false,
-    entities: [ "entity/*.ts"  ],
+    entities: [ ...(Object.keys(entities) ) ],
     migrations: [],
     subscribers: [],
     extra: {
