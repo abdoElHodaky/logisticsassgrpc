55bc5f3e79fbd75a0fe9d8d4442b3a1d3d6426fa
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-25 00:32:19 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -45,7 +45,7 @@ setInterval(function (){
 if(_AppDataSource.isConnected==false)
 {
   _AppDataSource.connect().then(e=>{
-  console.log("connected",e)
+  console.log("connected")
   }).catch(console.log)
 }
 else{
