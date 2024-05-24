66ce0e1167cf545e4416703bbc3a735193758dad
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-24 18:26:43 +0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ import * as entities from "./entity/";
 const redisconn = require('redis-url').parse("rediss://red-cp4soqocmk4c73eom0p0:kLoGjFxqLJRRHFQs1QUaImdvOtnNdF19@oregon-redis.render.com:6379")
 //console.log([Object.keys(entities)])
 export const _AppDataSource = new DataSource({
-    type: "mysql",
+    type: "mariadb",
     host:"sql.db4free.net",
     port:3306,
     username:"abdo_grpcendpoin",
