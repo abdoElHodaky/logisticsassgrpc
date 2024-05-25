085fcd73fbdb80592c7543e26f03d601ac609851
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-25 23:57:39 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -1,5 +1,9 @@
 #!/user/bin/bash
 
+if [[ "$ENABLE_OVERCOMMIT_MEMORY" == 'true' ]]; then
+	echo "vm.overcommit_memory=1" | sudo tee -a /etc/sysctl.conf
+  sudo sysctl -p
+fi
 npm-run-all build swaggergen 
 
 #node ./dist/grpc-reflect.js
