27e6b622666dd1544fc1c4b516e0d1ad87732337
AbdElrhman ElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2024-05-25 20:46:19 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ let cacheWithRedis = apicache.options({ redisClient: new Redis() }).middleware
 app.use(urlencoded({extended: true}))
 app.use(cors())
 app.use(json())
-app.use(cacheWithRedis("15 minutes"))
+app.use(cacheWithRedis("15 minutes",(req:any,res:any)=>res.statusCode===200)
 app.use(apiv1)
 /*app.use(rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
