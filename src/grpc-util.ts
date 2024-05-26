90e7e159298a01431bd5fda7eb4c375847f39925
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-26 18:07:46 +0300

    Update grpc-util.ts

--- src/grpc-util.ts
+++ src/grpc-util.ts
@@ -1,1 +1,13 @@
+import {
+  Server,
+  type UntypedServiceImplementation,
+} from "@grpc/grpc-js";
 
+export function addServiceToServer(
+  server:Server,
+  servicesImpls:Array<UntypedServiceImplementation>
+){
+  for (var impl of servicesImpls){
+    
+  }
+}
