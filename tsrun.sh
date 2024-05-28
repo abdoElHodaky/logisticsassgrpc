d1336c6b94cbd86e464241541c60fe40811cac25
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-28 20:11:08 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -1,8 +1,9 @@
 #!/user/bin/bash
 
 if [[ "$ENABLE_OVERCOMMIT_MEMORY" == 'true' ]]; then
-	echo "vm.overcommit_memory=1" | sudo tee -a /etc/sysctl.conf
-        sudo sysctl -p
+	#echo "vm.overcommit_memory=1" | sudo tee -a /etc/sysctl.conf
+        #sudo sysctl -p
+	sudo sysctl vm.overcommit_memory=1
 	echo "__"
 fi
 npm-run-all build swaggergen 
