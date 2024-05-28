b7974325b21617126c9ac3b16689b6f375e45492
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-28 21:41:51 +0300

    Update validated.ts

--- src/middlewares/validated.ts
+++ src/middlewares/validated.ts
@@ -6,11 +6,10 @@ export class ValidatedMiddleware implements Middleware {
   async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const {body}= request
     console.log(body)
-   /* if(typeof(req?.body))
+   // if(typeof(req?.body))
     const errors=await validate(req?.body)
-    if(errors) res.status(400).jsonp(errors)
-    */
-      
+    if(errors!=undefined) res.status(400).jsonp(errors)
+    
     next();
   }
 }
