879afb99d662c49804af156a95bf7eeed11ecd4d
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-29 13:41:34 +0300

    Update validated.ts

--- src/middlewares/validated.ts
+++ src/middlewares/validated.ts
@@ -3,12 +3,12 @@ import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
 export class ValidatedMiddleware implements Middleware {
-  async use(request: JWTRequest, res: Response, next: NextFunction): Promise<void> {
-    const {body}= request
+  async use(req: JWTRequest, res: Response, next: NextFunction): Promise<void> {
+    const {body}= req
     console.log(body)
    // if(typeof(req?.body))
     const errors=await validate(req?.body)
-    if(errors!=undefined || errors.length!=0) res.status(400).jsonp(errors)
+    if(errors!=undefined || errors?.length!=0) res.status(400).jsonp(errors)
     
     next();
   }
