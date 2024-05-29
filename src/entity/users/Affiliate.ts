37bf3012a8222601fd27bda80169d43ed6197fdf
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-29 19:53:59 +0300

    Update Affiliate.ts

--- src/entity/users/Affiliate.ts
+++ src/entity/users/Affiliate.ts
@@ -1,18 +1,23 @@
 import { type } from "os"
 import { Entity, PrimaryGeneratedColumn, Column, 
         OneToMany, TableInheritance ,ChildEntity,CreateDateColumn,
-        UpdateDateColumn ,BeforeInsert } from "typeorm"
+        UpdateDateColumn ,BeforeInsert, JoinColumn  } from "typeorm"
 import { User} from "./";
 import { genCode} from "../../helpers";
 
 @ChildEntity()
 export class Affiliate extends User {
-    /*
+    
     @Column({default:"marketer"})
     type:string
 
-    @ManyToOne(()=>User,referedBy=>referedBy.referals) referedBy:User
-    */
+    @ManyToOne(()=>User)
+    @JoinColumn([
+    { name: "referedBy", referencedColumnName: "referralCode" },
+    //{ name: "locale_id", referencedColumnName: "locale_id" }
+    ])
+    related:User
+        
     @Colummn()
     referralCode:string
 
