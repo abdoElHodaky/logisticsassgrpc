e6726eee5d9bc2ebdfb629bf94d9571a814c2730
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-29 13:39:20 +0300

    Update supTicket.ts

--- src/entity/supTicket.ts
+++ src/entity/supTicket.ts
@@ -1,5 +1,5 @@
 import { Entity ,Column,PrimaryGeneratedColumn, ManyToOne, CreateDateColumn, UpdateDateColumn  } from "typeorm"
-import { User } from "./User"
+import { User } from "./users/"
 
 @Entity()
 export class supTicket {
