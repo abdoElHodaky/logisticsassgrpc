03430edea7ccc7dcdff730fdbbf8fb90c529a4fe
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-29 10:54:41 +0300

    Create Author.ts

---
+++ src/entity/users/Author.ts
@@ -0,0 +1,14 @@
+import { type } from "os"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
+import { Email } from "../Email"
+import { User,Article} from "../"
+
+@ChildEntity()
+export class Author extends User {
+    
+    @Column({default:"author"})
+    type:string
+
+    @OneToMany(()=>Article,article=>article.author) articles:Article[];
+
+}
