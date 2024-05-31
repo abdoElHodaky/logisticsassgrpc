ae7655650a8d2e0a8f350138466ddbd0951d09e8
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-31 21:41:38 +0300

    Update auth.grpc.ts

--- src/controllers/auth.grpc.ts
+++ src/controllers/auth.grpc.ts
@@ -6,7 +6,7 @@ import { Response  } from "express";
 import { LoginUserDto,validatorDto } from "../dto/";
 import { Error } from "common-errors";
 import { isEmpty} from "../helpers";
-//import {ValidatedMiddleware} from "../middlewares/";
+import {ValidatedLogin} from "../middlewares/";
 import {Env} from "../env";
 import {validateOrReject,validate} from "class-validator";
 const address = "localhost:"+Env.GRPCSONEPORT
@@ -18,10 +18,10 @@ export class GrpcAuthController {
     credentials.createInsecure()
   )
   
-  @Post("/login",[  ])
+  @Post("/login",[ValidatedLogin  ])
   async login(@Res() res:Response, @Body() loginUserDto:LoginUserDto ):Promise<void>{
   const secret=Env.JWT_SECRET || "secret"
-    try{
+   /* try{
     const empty=isEmpty(loginUserDto)
     const errors=await validatorDto(LoginUserDto,loginUserDto)
       console.log(errors)
@@ -67,7 +67,39 @@ export class GrpcAuthController {
   catch(err:any){
    // const error=new Error("Login Information not provided or not existed",err)
     res.status(400).json({message:Object(errors).values()})
-  }
+  }*/
+   const req:_Auth.LoginUserReq={
+      username:loginUserDto.username,
+      passwordHash:loginUserDto.passwordHash
+    }
+    try{
+    this.client.login(req,(err:any,resp:_Auth.LoginUserRes)=>{
+      if (err) {
+      res.jsonp(err);
+        console.error(err)
+    } else {
+        
+        let user=resp.user
+        if(user!=undefined){
+       let token =jwt.sign({
+         username:user.username,
+         passwordHash:user.passwordHash,
+         type:user.type,
+         id:user.id
+       }, secret, { expiresIn: "1h"});
+       res.json({accessToken:token})
+     }
+        else {
+          res.json({accessToken:""})
+        }
+      }
+    })
+    }catch(err:any){
+      console.log(err)
+      //const error=new Error("Login Information not provided or not existed",err)
+      //res.status(400).json({message:Object(errors).values()})
+      }
+    
 }
  
 }
