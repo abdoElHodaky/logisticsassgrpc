8f0b9286c37dbb36887953ada854a309964bda18
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-31 09:16:43 +0300

    Update login-user.dto.ts

--- src/dto/login-user.dto.ts
+++ src/dto/login-user.dto.ts
@@ -1,8 +1,13 @@
-import { IsNotEmpty} from "class-validator";
+import { IsNotEmpty,validate,ValidationError} from "class-validator";
 export class LoginUserDto {
   @IsNotEmpty()
   readonly username:string
  // readonly email:string
   @IsNotEmpty()
   readonly passwordHash:string
+
+async  _validate():Promise<Array<any>>{
+   return  await validate(this)
+ }
+
 }
