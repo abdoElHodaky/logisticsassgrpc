fff7e65255a651ef508b4dd1a989686a0b319d22
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-31 22:48:00 +0300

    Update auth.grpc.ts

--- src/controllers/auth.grpc.ts
+++ src/controllers/auth.grpc.ts
@@ -21,8 +21,8 @@ export class GrpcAuthController {
   @Post("/login",[ValidatedLogin  ])
   async login(@Res() res:Response, @Body() loginUserDto:LoginUserDto ):Promise<void>{
   const secret=Env.JWT_SECRET || "secret"
-   /* try{
-    const empty=isEmpty(loginUserDto)
+    try{
+    //const empty=isEmpty(loginUserDto)
     const errors=await validatorDto(LoginUserDto,loginUserDto)
       console.log(errors)
     if(errors=={}){
@@ -67,39 +67,9 @@ export class GrpcAuthController {
   catch(err:any){
    // const error=new Error("Login Information not provided or not existed",err)
     res.status(400).json({message:Object(errors).values()})
-  }*/
+  }
    
-    try{
-      const req:_Auth.LoginUserReq={
-      username:loginUserDto.username,
-      passwordHash:loginUserDto.passwordHash
-    }
-    this.client.login(req,(err:any,resp:_Auth.LoginUserRes)=>{
-      if (err) {
-      res.jsonp(err);
-        console.error(err)
-    } else {
-        
-        let user=resp.user
-        if(user!=undefined){
-       let token =jwt.sign({
-         username:user.username,
-         passwordHash:user.passwordHash,
-         type:user.type,
-         id:user.id
-       }, secret, { expiresIn: "1h"});
-       res.json({accessToken:token})
-     }
-        else {
-          res.json({accessToken:""})
-        }
-      }
-    })
-    }catch(err:any){
-      console.log(err)
-      //const error=new Error("Login Information not provided or not existed",err)
-      //res.status(400).json({message:Object(errors).values()})
-      }
+    
     
 }
  
