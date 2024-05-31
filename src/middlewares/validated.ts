077ebd5b0d11fe627e9aa50b678a53ea0ad6c588
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-31 21:37:51 +0300

    Update validated.ts

--- src/middlewares/validated.ts
+++ src/middlewares/validated.ts
@@ -1,12 +1,25 @@
 import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
-import { Dto} from "../dto/Dto";
-export class ValidatedMiddleware implements Middleware {
+import { LoginUserDto,ValidatorDto} from "../dto/Dto";
+import { Error } from "common-errors";
+export class ValidatedLogin implements Middleware {
   
   async use(req: JWTRequest, res: Response, next: NextFunction): Promise<void> {
     const {body}= req
-    
-     next()
+   try{
+    const errs=await ValidatorDto(LoginUserDto,body)
+    if(errs=={}){
+      next()
+    }
+    else{
+      throw new Error("Login Information not provided or not existed")
+    }
+  }catch(err:any){
+     res.status(400).json({messages:Object(errors).values()})
+   }
   }
+
+
+  
 }
