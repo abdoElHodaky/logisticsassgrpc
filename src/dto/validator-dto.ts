86cd5b59310a2846158ac337b3f4aadd619a64eb
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-05-31 21:45:07 +0300

    Update validator-dto.ts

--- src/dto/validator-dto.ts
+++ src/dto/validator-dto.ts
@@ -7,7 +7,7 @@ export const validatorDto = async <T extends ClassConstructor<any>>(
 ) => {
   // tranform the literal object to class object
   const objInstance = plainToClass(dto, obj);
-  console.log(typeof(obj))
+  //console.log(typeof(obj))
   // validating and check the errors, throw the errors if exist
   const errors = await validate(objInstance);
   // errors is an array of validation errors
