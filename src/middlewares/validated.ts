362bc385321ba3e09f9fa0ece46bbfee21699e8f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-01 11:56:05 +0300

    Update validated.ts

--- src/middlewares/validated.ts
+++ src/middlewares/validated.ts
@@ -1,7 +1,7 @@
 import { Middleware } from '@decorators/express';
 import {Request,Response,NextFunction  } from "express";
 import { Request as JWTRequest } from "express-jwt";
-import { LoginUserDto,validatorDto} from "../dto/";
+import { LoginUserDto,validatorDto, CreateArticleDto } from "../dto/";
 import { Error } from "common-errors";
 export class ValidatedLogin implements Middleware {
   
@@ -10,6 +10,27 @@ export class ValidatedLogin implements Middleware {
   
     // console.log(typeof(body))
     const errors=await validatorDto(LoginUserDto,body)
+     console.log(errors)
+    if (errors!=[]){
+      res.status(400).jsonp({
+        FiledsMessages:errors.map({constrains}=>Object.values).join(" ,")
+      })
+    }
+    next()
+  }
+
+
+  
+}
+
+
+export class ValidatedCreatedArticle implements Middleware {
+  
+  async use(req: JWTRequest, res: Response, next: NextFunction): Promise<void> {
+    const {body}= req
+  
+    // console.log(typeof(body))
+    const errors=await validatorDto(CreateArticleDto,body)
      console.log(errors)
     if (errors!=[]){
       res.status(400).jsonp({
