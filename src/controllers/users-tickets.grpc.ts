0c2cca785ab59b8fee4677b7e6659c5e6c42f24f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-01 18:13:28 +0300

    Update users-tickets.grpc.ts

--- src/controllers/users-tickets.grpc.ts
+++ src/controllers/users-tickets.grpc.ts
@@ -28,10 +28,7 @@ export class GrpcUserTicketController {
         console.error(err)
     } else {
         const resl=_Ticket.GetAllTicketRes.toJSON(resp)
-        resl?.tickets?.forEach((ticket:object,inx:number)=>{
-        ticket["createdAt"]=dateToReadable(ticket["createdAt"])
-        ticket["updatedAt"]=dateToReadable(ticket["updatedAt"])
-        })
+        
         res.json(resl)
      }
     })
