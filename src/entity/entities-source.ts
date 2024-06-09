d248b8dd9004bd8124eb48a5925e392c9479d916
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-09 20:24:02 +0300

    Update entities-source.ts

--- src/entity/entities-source.ts
+++ src/entity/entities-source.ts
@@ -1,10 +1,10 @@
 import {
   Article,Attachment,Product,ProductAttachment,
-  Author,User,Affiliate,Purshase,
+  Author,User,Affiliate,Purshase,Supplier,
   PurshaseItem,Payment,supTicket,Verification} from "./";
 
 export const all=[
   Article,Attachment,Product,ProductAttachment,
   Author,User,Purshase,PurshaseItem,Payment,
-  supTicket,Verification,Affiliate
+  supTicket,Verification,Affiliate,Supplier
 ]
