4a06347ceff08569c351e31c6fff2a5155505150
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-09 20:16:51 +0300

    Update products.ts

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,1 +1,28 @@
+import { _Data } from "./datasource";
+import { Product ,User } from "../entity/"
+//import { CreateArticleDto } from "../dto/create-article.dto"
 
+//@Injectable()
+export class ProductService extends _Data {
+  constructor (){
+      super()
+  }
+
+  async all():Promise<Product[]>
+  {
+    //console.log(this._source)
+    return await this.datasource.manager.find(Product,{
+      relations:{
+        user:true
+      },
+      cache:true
+    })
+  }
+
+// async create(userId:string,ticket:{type:string,subject:string,description:string}):Promise<supTicket|void>{
+    
+    
+    
+    
+ // }
+}
