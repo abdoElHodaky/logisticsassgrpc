472b5b247ba5126626d4fcbea7bc05ec719ed356
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-09 20:19:40 +0300

    Update Supplier.ts

--- src/entity/users/Supplier.ts
+++ src/entity/users/Supplier.ts
@@ -1,1 +1,14 @@
+import { type } from "os"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
+import { User} from "./";
+import {Product} from "../"
 
+@ChildEntity()
+export class Supplier extends User {
+    
+    @Column({default:"Supplier"})
+    type:string
+
+    @OneToMany(()=>Product,product=>product.supplier) products: Product[];
+
+}
