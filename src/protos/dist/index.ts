c4e81ad80e1e1ff26d8239342ab015acc92f05aa
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-09 20:06:43 +0300

    Update index.ts

--- src/protos/dist/index.ts
+++ src/protos/dist/index.ts
@@ -2,3 +2,5 @@ export * as _Auth from "./auth";
 export * as _User from "./user";
 export * as _Ticket from "./ticket";
 export * as _Article from "./article";
+export * as _Payment from "./payment";
+export * as _Purshase from "./purshase";
