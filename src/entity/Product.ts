2bc9ae26f41d642f0428d15349d3f5c3fb7c2e65
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-10 15:24:28 +0300

    Update Product.ts

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,5 +1,5 @@
-import { Entity ,Column,PrimaryGeneratedColumn, OneToMany, ManyToOne, ChildEntity, CreateDateColumn, UpdateDateColumn  } from "typeorm"
-import { Supplier,Attachment} from "./";
+import { Entity ,Column,PrimaryGeneratedColumn,OneToOne, OneToMany, ManyToOne, ChildEntity, CreateDateColumn, UpdateDateColumn  } from "typeorm"
+import { Supplier,Attachment,PurchasedItem} from "./";
 import { ProductAttachment } from "./Attached";
 @Entity()
 //@ChildEntity()
@@ -19,7 +19,7 @@ export class Product {
     @OneToMany(()=>ProductAttachment,attachment=>attachment.attached) attachments:Attachment[];
     @ManyToOne(()=>Supplier,supplier=>supplier.products) supplier:Supplier;
     @OneToMany(()=>subProduct,subproduct=>subproduct.parent) subs:subProduct[];
-   
+    @OneToOne(()=>PurshasedItem,item=>item.product) item:PurshasedItem
    
 }
 
