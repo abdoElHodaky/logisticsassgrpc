a63c6ccdd8ed900782c90ad40f190a77bef964ac
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-10 20:02:50 +0300

    Update swagautogen.js

--- swagautogen.js
+++ swagautogen.js
@@ -83,7 +83,7 @@ const config = {
             },
             userAddTicket:{
                // $userId:"4",
-                $type:"inquiry|complaint",
+                $type:{schema:{@enum:["inquiry","complaint"]}},
                 $subject:"greet",
                 $description:"how are you?"
             },
