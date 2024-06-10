eae994aedc74d6f5e7a76d013ffaba4f2034c7c1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-10 20:36:59 +0300

    Update util.ts

--- src/grpc/util.ts
+++ src/grpc/util.ts
@@ -24,7 +24,7 @@ export function dateToReadable(date:Date|string){
 }
 
 export function transformDate<T extends ClassConstructor<any>>(entity:T,fieldsnames:string[]):object{
-  let _obj=classToPlain(entity,obj)
+  let _obj=classToPlain(T,entity)
   fieldsnames.forEach(e=>{
     _obj[e]=dateToReadable(_obj[e])
   })
