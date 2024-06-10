c0396ea6c805158b9cf8d75363ddd8f7dec214e6
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-10 19:57:26 +0300

    Create productroute-grpc.ts

---
+++ src/routes/productroute-grpc.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+//import { Author } from "../entity/Author";
+//import { Article } from "../entity/Article"
+//import { AppDataSource } from "../_datasource";
+
+export const productsgrpc=Router()
+
+productsgrpc.get("/products",(req,res)=>{
+    /* 	#swagger.tags = ['GRPC.Product']
+        #swagger.description = 'Endpoint to get articles via grpc'
+        
+        */
+
+})
+
+    
+
+productsgrpc.post("/products",({body},res)=>{
+    /* 	#swagger.tags = ['GRPC.Product']
+        #swagger.description = 'Endpoint to add new article via grpc' 
+        #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: { $ref: '#/components/schemas/AddArticle' }  }
+                }
+            }
+        } 
+       #swagger.security = [{
+            "JWTAuth": []
+       }]
+    */
+    
+   
+})
+
+/*articlesroute.post("/articles/create",(req,res)=>{
+    
+})
+*/
