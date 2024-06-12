e79f4af03d10ec3f2f841759fe1841fd0059db09
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-13 00:56:23 +0300

    Update Org.ts

--- src/entity/orgs/Org.ts
+++ src/entity/orgs/Org.ts
@@ -1,1 +1,43 @@
+import { type } from "os"
+import { Entity, PrimaryGeneratedColumn, JoinColumn, JoinTable ,Column, OneToMany, TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
+//import { Email } from "./Email"
+import { User,Address} from "../"
 
+@Entity()
+@TableInheritance({column:{type:"varchar",name:"type"}})
+//@TableInheritance({column:{type:"varchar",name:"referralCode"}})
+//@TableInheritance({column:{type:"varchar",name:"referedBy"}})
+
+export class Org {
+
+    @PrimaryGeneratedColumn("increment")
+    id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    description: string
+    
+    @Column(()=>Address)
+    address:Address
+    
+    @CreateDateColumn({type:"date"})
+    createdAt: Date;
+
+    @UpdateDateColumn({type:"date"})
+    updatedAt: Date;
+    
+   /* @OneToMany(()=>supTicket,ticket=>ticket.user) tickets:supTicket[]
+    @OneToMany(()=>Verification,verification=>verification.user) verifications:Verification[];
+    @OneToMany(()=>Attachment,media=>media.uploader) media:Attachment[]
+    @OneToMany(()=>Payment,payment=>payment.user) payments:Payment[]
+    @OneToMany(()=>Purshase,purchase=>purchase.user) purchases:Purshase[]
+    @OneToMany(()=>Affiliate,affiliates=>affiliates.related)
+    @JoinColumn([
+    { name: "referedBy", referencedColumnName: "referralCode" },
+    //{ name: "locale_id", referencedColumnName: "locale_id" }
+    ])
+    affiliates:Affiliate[]*/
+   
+}
