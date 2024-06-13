94245dd7ed5180a917305bb9175700c76e72bf4b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-13 18:40:29 +0300

    Update orgzs.ts

--- src/services/orgzs.ts
+++ src/services/orgzs.ts
@@ -1,1 +1,36 @@
+import { _Data } from "./datasource";
+import { Orgz ,Owner } from "../entity/"
+import { Error , NotFoundError } from "common-errors";
+import { isNumeric } from "../helpers";
 
+//import { CreateArticleDto } from "../dto/create-article.dto"
+
+//@Injectable()
+export class OrgzService extends _Data {
+  constructor (){
+      super()
+  }
+
+  async all(userId?:number):Promise<Orgz[]|Error>
+  {
+  
+    const orgzs= await this.datasource.manager.find(Orgz,{
+      where:(userId!=undefined)?{owner:{id:userId}}:{},
+      relations:{
+        owner:true
+      },
+      cache:true
+    })
+    return (orgzs.length!=0)? orgzs : new NotFoundError("Products")
+  }
+
+async create(userId:number,orgz:{type:string,title:string,description:string}):Promise<Orgz|void>{
+    let _orgz=await this.datasource.manger.create(Orgz,{
+      ...orgz
+    })
+   return _orgz
+    
+    
+    
+ }
+}
