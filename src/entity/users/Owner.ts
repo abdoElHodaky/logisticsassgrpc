7798783607e0051dbd7f1864d0859d4bce816f7e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-13 10:21:55 +0300

    Update Owner.ts

--- src/entity/users/Owner.ts
+++ src/entity/users/Owner.ts
@@ -1,1 +1,14 @@
+import { type } from "os"
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
+import { User} from "./";
+import {Org} from "../"
 
+@ChildEntity()
+export class Owner extends User {
+    
+    @Column({default:"Owner"})
+    type:string
+
+   // @OneToMany(()=>Product,product=>product.supplier) products: Product[];
+
+}
