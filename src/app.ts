edb7e920958608e7c326fd536c8c7e4ee3dd308e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-14 22:37:08 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { json,urlencoded,Request } from "express";
 import { slowDown } from 'express-slow-down'
 import { RedisStore } from 'rate-limit-redis'
 import cors from "cors";
-import apicache from 'apicache'
+//import apicache from 'apicache'
 import { AppDataSource } from "./_datasource";
 
 import { apiv1 } from "./routes";
@@ -17,16 +17,16 @@ const redisClient = redis.createClient({
        pingInterval: 10000
 });
 
-redisClient.connect().then(console.log).catch(console.error);
+//redisClient.connect().then(console.log).catch(console.error);
 const app=application();
 const port = process.env.PORT||4000
 const { SwaggerTheme, SwaggerThemeNameEnum } = require('swagger-themes');
 const expressPrettier = require('express-prettier')
 const theme = new SwaggerTheme();
-let cacheWithRedis = apicache.options({ redisClient:/* redis.createClient({
+l/*et cacheWithRedis = apicache.options({ redisClient:/* redis.createClient({
 	url:process.env.REDIS,legacyMode: true
 	//url:"redis://red-cp4soqocmk4c73eom0p0:kLoGjFxqLJRRHFQs1QUaImdvOtnNdF19@oregon-redis.render.com:6379"
-})*/ redisClient }).middleware
+})redisClient }).middleware */
 const limiter = slowDown({
 	windowMs: 5 * 60 * 1000, // 15 minutes
 	delayAfter: 5, // Allow 5 requests per 15 minutes.
@@ -42,7 +42,7 @@ app.set('trust proxy', 2)
 app.use(urlencoded({extended: true}))
 app.use(cors())
 app.use(json())
-app.use(cacheWithRedis("15 minutes",(req:any,res:any)=> res.statusCode===200))
+//app.use(cacheWithRedis("15 minutes",(req:any,res:any)=> res.statusCode===200))
 app.use(limiter)
 app.use(apiv1)
 app.use(
