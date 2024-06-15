eff895fb15e3b682e14d6cac8eea5af895d550bf
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-15 12:40:08 +0300

    Update orgzs.grpc.ts

--- src/services/orgzs.grpc.ts
+++ src/services/orgzs.grpc.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { sendUnaryData, ServerUnaryCall, status, UntypedHandleCall ,handleUnaryCall} from "@grpc/grpc-js";
 import  {_Orgz} from "../protos/dist/";
-import { services }from "./";
+import { services }from "./enum";
 import { Orgz } from "../entity/";
 //console.log(services)
 export class orgzGrpcService  {
@@ -10,17 +10,38 @@ export class orgzGrpcService  {
   static service:any=services.Orgz
   //public [name: string]:UntypedHandleCall;
   public SrvImpl: _Orgz.OrgzServiceServer = {
-   async all (call: ServerUnaryCall<_Orgz.GetAllOrgzsReq,_Orgz.GetAllOrgzsRes>,
+   
+     async create (
+    call: ServerUnaryCall<_Orgz.CreateOrgzReq,_Orgz.CreateOrgzRes>,
+    callback: sendUnaryData<_Orgz.CreateOrgzRes>
+  ){
+       let {userId,orgz}=call.request
+       const supticket=_Orgz.Orgz.toJSON((orgz!=undefined)?orgz:_Orgz.createBaseOrgz())
+       let _orgz=await orgzGrpcService.service.create(userId,orgz)
+       if(_orgz instanceof Orgz){
+        const orgz=_Orgz.Orgz.fromJSON(_orgz)
+         orgz.OwnerId=parseInt(userId)
+         callback(null,{
+           orgz:orgz
+         })
+       }
+      else{
+        callback(null,{
+           ticket:_Orgz.createBaseOrgz()
+         })
+      }
+     },
+    async all (call: ServerUnaryCall<_Orgz.GetAllOrgzsReq,_Orgz.GetAllOrgzsRes>,
     callback: sendUnaryData<_Orgz.GetAllOrgzsRes>
  ){
      let orgzs=await orgzGrpcService.service.all()
      //console.log(tickets)
-     let _orgzs=tickets.map(_Ticket.Ticket.fromJSON)
+     let _orgzs=tickets.map(_Orgz.Orgz.fromJSON)
      //console.log(tickets)
     _orgzs.forEach((a:_Orgz.Orgz,inx:number)=>{
      let {user,createdAt,updatedAt}=orgzs[inx]
      if(user!=null) {
-      a.ownerId=user.id
+      a.OwnerId=user.id
       //console.log(a.createdAt instanceof Date)
      }
    // else { a.userId = Math.floor(Math.random()*21) }
@@ -32,26 +53,6 @@ export class orgzGrpcService  {
      }}
      callback(null,res)
       }
-    , async create (
-    call: ServerUnaryCall<_Orgz.CreateOrgzReq,_Orgz.CreateOrgzRes>,
-    callback: sendUnaryData<_Orgz.CreateOrgzRes>
-  ){
-       let {userId,orgz}=call.request
-       const supticket=_Orgz.Orgz.toJSON((orgz!=undefined)?orgz:_Orgz.createBaseOrgz())
-       let _orgz=await orgzGrpcService.service.create(userId,orgz)
-       if(_orgz instanceof Orgz){
-        const orgz=_Orgz.Orgz.fromJSON(_orgz)
-         orgz.owner=parseInt(userId)
-         callback(null,{
-           orgz:orgz
-         })
-       }
-      else{
-        callback(null,{
-           ticket:_Orgz.createBaseOrgz()
-         })
-      }
-     }
   }
 
   
