593341bedf061c152e9230d962ae4e81c00f4423
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-15 09:24:40 +0300

    Update Attached.ts

--- src/entity/Attached.ts
+++ src/entity/Attached.ts
@@ -1,10 +1,17 @@
 import { ChildEntity,ManyToOne,Column} from "typeorm";
-import { Attachment} from "./Attachment";
-import { Product} from "./Product";
+import { Attachment,Product,Orgz} from "./";
 @ChildEntity()
 export class  ProductAttachment extends Attachment {
     @Column({default:Product.name})
     forType:string
     @ManyToOne(()=>Product,attached=>attached.attachments) attached:Product;
+   
+} 
+
+@ChildEntity()
+export class  OrgzAttachment extends Attachment {
+    @Column({default:Orgz.name})
+    forType:string
+    @ManyToOne(()=>Orgz,attached=>attached.attachments) attached:Orgz;
    
 } 
