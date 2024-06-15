04d0820baa1baa41d7427e8e88ddba1fbd3021b3
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-15 10:30:55 +0300

    Update orgzs.grpc.ts

--- src/controllers/orgzs.grpc.ts
+++ src/controllers/orgzs.grpc.ts
@@ -1,5 +1,5 @@
-import { Orgz } from "../entity/"
-import { AppDataSource } from "../_datasource";
+import { credentials } from "@grpc/grpc-js";
+import {_Orgz } from "../protos/dist/";
 import { AuthenticateMiddleware } from "../middlewares/"
 //import { CreatePaymentDto} from "../dto/create-payment.dto";
 import { Res, Post, Controller, Get, Body , Params ,Delete,Req,Query } from '@decorators/express';
@@ -7,22 +7,36 @@ import { Response} from "express"
 import { Request } from "express-jwt";
 import { isNumeric,nationalIdvalid } from "../helpers";
 import { Error , NotFoundError } from "common-errors";
+import {Env} from "../env";
 import {services} from "../services/enum";
-
+const address = "localhost:"+ Env.GRPCSTWOPORT
 @Controller('/products')
 export class GrpcProductController {
-  
+  private client =new _Orgz.OrgzServiceClient(
+    address,
+    credentials.createInsecure()
+  )
   private service=services.Orgz
   
   constructor(){}
   
   @Get("",[])
-  async all(@Req() req:Request, @Res() res:Response):Promise<Orgz[]|void>{
-    const {auth}=req
-    console.log(auth)
-    let orgzs=await this.service.all()
-    if ((orgzs instanceof Array ) && orgzs.length!=0 ) return orgzs
-    else res.jsonp({message:orgzs?.message})
+  async all(@Res() res:Response ):Promise<void>{
+   // console.log("89")
+    const req:_Orgz.GetAllOrgzReq={  
+      
+    }
+    console.log(req)
+    this.client.all(req,(err:any,resp:_Ticket.GetAllTicketRes)=>{
+      if (err) {
+      res.jsonp(err);
+        console.error(err)
+    } else {
+        const resl=_Orgz.GetAllOrgzRes.toJSON(resp)
+        
+        res.json(resl)
+     }
+    })
   }
   
   @Post("",[AuthenticateMiddleware])
