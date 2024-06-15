164eb6dac07d01ad58792682159508b9f63d734c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-15 09:30:36 +0300

    Update create-article.dto.ts

--- src/dto/create-article.dto.ts
+++ src/dto/create-article.dto.ts
@@ -1,11 +1,13 @@
+import { IsNotEmpty} from "class-validator";
+
 export class CreateArticleDto{
 
-
+@IsNotEmpty()
 readonly  cateogry:string
+@IsNotEmpty()
 readonly  imgurl:string
+@IsNotEmpty()
 readonly  content:string
+@IsNotEmpty()
 readonly title:string
-//readonly  userId:number
-
-
 }
