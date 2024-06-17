01088e4aa5a97cdea5914291e601f72835e41118
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 11:06:13 +0300

    Update Subscribed.ts

--- src/entity/products/Subscribed.ts
+++ src/entity/products/Subscribed.ts
@@ -1,8 +1,8 @@
 import { Entity ,Column,PrimaryGeneratedColumn,OneToOne, OneToMany, 
         ManyToOne, ChildEntity, CreateDateColumn, UpdateDateColumn  } from "typeorm"
-
+import {Subscription} from "./Subscription";
 @ChildEntity()
 export class SubscribedProduct extends Product {
-   // @ManyToOne(()=>Product,product=>product.subs) parent:Product;
+    @ManyToOne(()=>Subscription,subscrip=>subscrip.products) subscrip?:Subscription;
    
 }
