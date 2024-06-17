0349be6e2b51d3300a751d69583a44f43b5298bc
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 18:03:47 +0300

    Update Child.ts

--- src/entity/products/Child.ts
+++ src/entity/products/Child.ts
@@ -1,10 +1,13 @@
 import { Entity ,Column,PrimaryGeneratedColumn,OneToOne, OneToMany, 
-        ManyToOne, ChildEntity, CreateDateColumn, UpdateDateColumn  } from "typeorm"
+        ManyToOne, ChildEntity, CreateDateColumn, UpdateDateColumn,JoinColumn  } from "typeorm"
 import { Product } from "./Product";
 
 @ChildEntity()
 export class ChildProduct extends Product {
 
-  @ManyToOne(()=>Product,parent=>parent.subs) parent:Product;
-   
+    @ManyToOne(()=>Product,related=>related.products)
+    @JoinColumn([
+    { name: "relatedId", referencedColumnName: "id" },
+    {} ])
+    related:Product
 }
