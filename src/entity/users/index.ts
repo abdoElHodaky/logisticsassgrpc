cc30e65f8ce0fef7beb66b6862108ad9a5182d65
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 11:50:16 +0300

    Update index.ts

--- src/entity/users/index.ts
+++ src/entity/users/index.ts
@@ -3,3 +3,4 @@ export { Author } from "./Author";
 export { Affiliate } from "./Affiliate";
 export { Supplier } from "./Supplier";
 export { Owner} from "./Owner";
+export { Subscriber} from "./Subscriber";
