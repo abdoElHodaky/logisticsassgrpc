d249506c9f38483d131eade28a198a32d92a365b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 10:58:55 +0300

    Update Subscription.ts

--- src/entity/Subscription.ts
+++ src/entity/Subscription.ts
@@ -1,1 +1,22 @@
+import { Entity,Column,ManyToOne,OneToMany,PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm"
+import { Subscriber } from "./users/";
+import { SubscribedProduct } from "./products/";
+@Entity()
+export class Subscription {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
 
+    @Column({type: "varchar", nullable: true})
+    title: string;
+
+    
+
+    @ManyToOne(()=>Subscriber,user=>user.subscrips) user:Subscribe;
+    @OneToMany(()=>SubscribedProduct,product=>product.subscrip) products:SubscribedProduct[];
+   
+    @CreateDateColumn({type:"date"})
+    createdAt: Date;
+
+    @UpdateDateColumn({type:"date"})
+    updatedAt: Date;
+}
