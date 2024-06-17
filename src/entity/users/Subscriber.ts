e36d9f03c3f1e750f359c89c34eecadbf87a2e2c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-17 10:53:52 +0300

    Update Subscriber.ts

--- src/entity/users/Subscriber.ts
+++ src/entity/users/Subscriber.ts
@@ -3,7 +3,6 @@ import { Entity, PrimaryGeneratedColumn, Column,
  ManyToMany,JoinTable, TableInheritance ,
         ChildEntity  } from "typeorm"
 import { SubscribedProduct } from "./";
-import {Orgz} from "../"
 
 @ChildEntity()
 export class Subscribe extends User {
