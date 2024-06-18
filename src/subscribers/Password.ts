44ffb50e07e097e85ef5de3c459f813cddcdf03d
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-18 23:28:58 +0300

    Update Password.ts

--- src/subscribers/Password.ts
+++ src/subscribers/Password.ts
@@ -1,1 +1,20 @@
+import { EntitySubscriberInterface, InsertEvent } from "typeorm"
+import { Password,User } from "../entity/";
+@EventSubscriber()
+export class PasswordSubscriber implements EntitySubscriberInterface<Password> {
+    /**
+     * Indicates that this subscriber only listen to Post events.
+     */
+    listenTo() {
+        return Password
+    }
 
+    /**
+     * Called before post insertion.
+     */
+  async  aferInsert(event: InsertEvent<Post>) {
+        const {entity,manager} =event
+        entity.user?.passwordHash=entity.passphase
+        await manager.save(User,entity)
+    }
+}
