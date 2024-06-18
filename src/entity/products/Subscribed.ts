aa658c9fdefe412606f668bee258a7c2e97438f4
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-18 11:21:23 +0300

    Update Subscribed.ts

--- src/entity/products/Subscribed.ts
+++ src/entity/products/Subscribed.ts
@@ -1,19 +1,9 @@
 import { Entity ,Column,PrimaryGeneratedColumn,OneToOne, OneToMany, JoinColumn,
         ManyToMany,JoinTable,ManyToOne, ChildEntity, CreateDateColumn, UpdateDateColumn  } from "typeorm"
-import {Subscription} from "../Subscription";
+import {Affiliate} from "../users/";
 import {Product} from "./Product";
-/*@ChildEntity()
-export class SubscribedProduct extends Product {
+@ChildEntity()
+export class AffiliateProduct extends Product {
         
-    @Column("int")
-    SProductId:number
-
-    @OneToMany(()=>Subscription,subscrip=>subscrip.products) 
-    //@JoinColumn({name:"id", referencedColumnName:"SProductId" })
-   
-    subscrip:Subscription; 
-
-        
-   
 }
-*/
+
