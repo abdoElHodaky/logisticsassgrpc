f2cef2d8fb97c4c3bf8d08f98641738a10f5cbb1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-18 22:26:52 +0300

    Update VerifyCode.ts

--- src/entity/verifications/VerifyCode.ts
+++ src/entity/verifications/VerifyCode.ts
@@ -1,1 +1,19 @@
+import { Column, Entity, ManyToOne,
+        PrimaryGeneratedColumn } from "typeorm"
 
+import { genCode} from "../../helpers";
+
+@Entity()
+export class VerifyCode{
+   @Column({type:"varchar"})
+   code:string
+  
+   @Column({type:"date"})
+   expiresAt:Date
+  
+   @ManyToOne(()=>Verification,verification=>verification.verify_codes)verification:Verification
+   @BeforeInsert()
+   setCode(){
+        this.code=genCode()
+    }
+}
