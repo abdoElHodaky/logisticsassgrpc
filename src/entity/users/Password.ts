46c2dd9bf534bb53729922165fd671ccba64487f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-18 22:50:46 +0300

    Update Password.ts

--- src/entity/users/Password.ts
+++ src/entity/users/Password.ts
@@ -1,1 +1,12 @@
+import { Entity,Column,ManyToOne,OneToMany,
+        PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm"
 
+@Entity()
+export class Password {
+  
+  @PrimaryGeneratedColumn("increment")
+  id:number;
+
+  @Column({type:"varchar"})
+  passphase:string;
+}
