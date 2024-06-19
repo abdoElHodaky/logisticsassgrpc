3941e8df1574bdd9511413955ce061b80b4a8ba3
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-19 10:49:51 +0300

    Update VerifyCode.ts

--- src/entity/verifications/VerifyCode.ts
+++ src/entity/verifications/VerifyCode.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, ManyToOne,
-        PrimaryGeneratedColumn } from "typeorm"
+        PrimaryGeneratedColumn,BeforeInsert } from "typeorm"
 
 import { genCode} from "../../helpers";
 
