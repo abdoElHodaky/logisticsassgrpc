43363a792dfcae19c0f535b5e48bf734e552a732
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-19 10:36:10 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -1,8 +1,8 @@
 #!/user/bin/bash
 
 
-npm-run-all build swaggen start
-#node dist/data-source.js
+npm-run-all build swaggen 
+node dist/data-source.js
 
 #node ./dist/grpc-reflect.js
 #npm-run-all --parallel start
