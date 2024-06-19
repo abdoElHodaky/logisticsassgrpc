7b37e4e20ce24973b45fd8e9bbf44e1651bf68cf
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-19 08:32:36 +0300

    Create Purshase.ts

---
+++ src/subscribers/Purshase.ts
@@ -0,0 +1,25 @@
+import { EntitySubscriberInterface, LoadEvent } from "typeorm"
+import { Purshase,User } from "../entity/";
+@EventSubscriber()
+export class PurshaseSubscriber implements EntitySubscriberInterface<Purshase> {
+    /**
+     * Indicates that this subscriber only listen to Post events.
+     */
+    listenTo() {
+        return Purshase
+    }
+
+    /**
+     * Called before post insertion.
+     */
+  async  aferLoad(event: LoadEvent<Purshase>) {
+        const {entity,manager} =event
+        if(entity.items.length!=0){
+          entity.items.reduce((sum,item)=>{
+           entity.subTotal+=item?.product.price
+           return sum
+          })
+        }
+        await manager.save(Purshase,entity)
+    }
+}
