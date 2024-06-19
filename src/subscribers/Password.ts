eed69e3fb88982bf886ebf99f269cffc18029260
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-19 11:42:00 +0300

    Update Password.ts

--- src/subscribers/Password.ts
+++ src/subscribers/Password.ts
@@ -14,7 +14,7 @@ export class PasswordSubscriber implements EntitySubscriberInterface<Password> {
      */
   async  aferInsert(event: InsertEvent<Password>) {
         const {entity,manager} =event
-        entity.user?.passwordHash=entity.passphase
+        entity.user.passwordHash=entity.passphase
         await manager.save(User,entity)
     }
 }
