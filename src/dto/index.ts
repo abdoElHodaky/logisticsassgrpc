74618330346416b2ea30fd3c7afd830b079516e5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-20 14:57:55 +0300

    Update index.ts

--- src/dto/index.ts
+++ src/dto/index.ts
@@ -4,4 +4,4 @@ export {CreateUserDto} from "./create-user.dto";
 export {LoginUserDto} from "./login-user.dto";
 export {CreatePurshaseDto,CreateSubscriptionDto} from "./create-purshase-subscribe.dto";
 export {validatorDto} from "./validator-dto";
-export {CreateProduct} from "./create-product";
+export {CreateProduct} from "./create-product.dto";
