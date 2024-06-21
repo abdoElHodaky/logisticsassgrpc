5910cef7d8bf528e9be6ad437858dc134a48fd57
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-21 20:46:35 +0300

    Update Sub.ts

--- src/entity/orgs/Sub.ts
+++ src/entity/orgs/Sub.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, JoinColumn,
         JoinTable ,Column, OneToMany, ManyToOne,
         TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
 //import { Email } from "./Email"
-import { Orgz} from "./"
+import { Orgz} from "./Org"
 
 @ChildEntity()
 export class subOrgz extends Orgz{
