61bf8470deb308136191913081a50a72eb4d8d00
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-21 19:47:07 +0300

    Update Sub.ts

--- src/entity/orgs/Sub.ts
+++ src/entity/orgs/Sub.ts
@@ -1,6 +1,6 @@
 import { type } from "os"
 import { Entity, PrimaryGeneratedColumn, JoinColumn,
-        JoinTable ,Column, OneToMany, ManyToOne,ChildEntity,
+        JoinTable ,Column, OneToMany, ManyToOne,
         TableInheritance ,ChildEntity,CreateDateColumn, UpdateDateColumn  } from "typeorm"
 //import { Email } from "./Email"
 import { Orgz} from "./"
