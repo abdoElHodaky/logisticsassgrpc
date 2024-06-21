7e8bdc43beff071edbfd9ee24892eb2dd2870301
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-21 19:00:14 +0300

    Update index.ts

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,7 +1,7 @@
 export { Article } from "./Article"
 export { Owner , Subscriber,Supplier,Password
         ,User,Affiliate,Author} from "./users/"
-export { Orgz } from "./orgs/";
+export * from "./orgs/";
 export { Purshase , PurshaseItem } from "./Purshase"
 export { supTicket } from "./supTicket"
 export { ProductAttachment,OrgzAttachment,Attachment } from "./attachments/";
