25ae9bfa5dce90cbb2249790691a55aa73fe907e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-22 19:04:11 +0300

    Update productsroute-grpc.ts

--- src/routes/productsroute-grpc.ts
+++ src/routes/productsroute-grpc.ts
@@ -4,10 +4,11 @@ import { Router } from "express";
 //import { AppDataSource } from "../_datasource";
 
 export const productsgrpc=Router()
+export const purshasesgrpc=Router()
 
 productsgrpc.get("/products",(req,res)=>{
     /* 	#swagger.tags = ['GRPC.Product']
-        #swagger.description = 'Endpoint to get articles via grpc'
+        #swagger.description = 'Endpoint to get products via grpc'
         
         */
 
@@ -17,7 +18,7 @@ productsgrpc.get("/products",(req,res)=>{
 
 productsgrpc.post("/products",({body},res)=>{
     /* 	#swagger.tags = ['GRPC.Product']
-        #swagger.description = 'Endpoint to add new article via grpc' 
+        #swagger.description = 'Endpoint to add new product via grpc' 
         #swagger.requestBody = {
             required: true,
             content: {
@@ -36,12 +37,38 @@ productsgrpc.post("/products",({body},res)=>{
 
 productsgrpc.post("/products/subscribe",({body},res)=>{
     /* 	#swagger.tags = ['GRPC.Product']
-        #swagger.description = 'Endpoint to add new article via grpc' 
+        #swagger.description = 'Endpoint to subscribe in product via grpc' 
         #swagger.requestBody = {
             required: true,
             content: {
                 "application/json": {
                   schema: { $ref: '#/components/schemas/subscribeProduct' }  }
+                }
+            }
+        } 
+       #swagger.security = [{
+            "JWTAuth": []
+       }]
+    */
+    
+   
+})
+purshasesgrpc.get("/purchases",(req,res)=>{
+    /* 	#swagger.tags = ['GRPC.Purshase']
+        #swagger.description = 'Endpoint to get purshases via grpc'
+        
+        */
+
+})
+
+purchasesgrpc.post("/purchases",({body},res)=>{
+    /* 	#swagger.tags = ['GRPC.Purshase']
+        #swagger.description = 'Endpoint to create in purshase via grpc' 
+        #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                  schema: { $ref: '#/components/schemas/CreatePurshase' }  }
                 }
             }
         } 
