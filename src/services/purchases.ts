3971c744974ebbe48a9ec6988a6d883556a20f9c
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-22 18:54:18 +0300

    Update purchases.ts

--- src/services/purchases.ts
+++ src/services/purchases.ts
@@ -30,12 +30,20 @@ export class PurshaseService extends _Data {
   
 async create(dto:CreatePurshaseDto ):Promise<Purshase|void>{
    const purchase=await this.datasource.manager.create(Purshase,{})
-   for(var i in dto?.items){
+   const {userId,itemsIds}=dto
+  /* for(var i in dto?.items){
      const item=new PurshaseItem()
      item.product=await this.ProductService.id(dto?.items[id])
      await this.datasource.manager.save(PurshaseItem,item)
      purchase.items.push(item)
-   }
+   }*/
+  const items= itemsIds.map(id=>{
+     const item=new PurshaseItem()
+     item.product=await this.ProductService.id(dto?.items[id])
+     return item
+   })
+   purchase.items.push(...items)
+   purchase.user=await this.userS.id(userId)
    return purchase
  } 
 
