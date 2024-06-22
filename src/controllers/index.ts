191bbd4026ea911d3b7deda0541067a939d31f95
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-22 19:40:06 +0300

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -9,9 +9,11 @@ import {GrpcMeController} from "./me";
 import {GrpcUserPaymentController} from "./users-payments.grpc";
 import {GrpcProductController} from "./products.grpc";
 import {GrpcOrgzController} from "./orgzs.grpc";
+import {PurshaseController} from "./purchases.grpc";
 export const controllers=[GrpcAuthController,GrpcAuthorController,
                           GrpcArticleController,GrpcUserTicketController,
                           GrpcUserController,GrpcSupTicketController,
                           PaymentController,GrpcMeController,GrpcOrgzController,
-                          GrpcUserPaymentController,GrpcProductController
+                          GrpcUserPaymentController,GrpcProductController,
+                          PurshaseController
                          ]
