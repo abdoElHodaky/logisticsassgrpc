56f4d261a3804299e3f7ba6a8b73342061e3c19e
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-24 00:31:02 +0300

    Update Payment.ts

--- src/subscribers/Payment.ts
+++ src/subscribers/Payment.ts
@@ -1,1 +1,21 @@
+import {EventSubscriber ,EntitySubscriberInterface, InsertEvent } from "typeorm"
+import { Payment,User } from "../entity/";
+@EventSubscriber()
+export class PaymentSubscriber implements EntitySubscriberInterface<Payment> {
+    /**
+     * Indicates that this subscriber only listen to Post events.
+     */
+    listenTo() {
+        return Payment
+    }
 
+    /**
+     * Called before post insertion.
+     */
+  async  aferInsert(event: InsertEvent<Payment>) {
+        const {entity,manager} =event
+        entity.user.passwordHash=entity.passphase
+        entity.user.passwords.push(entity)
+        await manager.save(User,entity)
+    }
+}
