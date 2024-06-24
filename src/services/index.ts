677bf0a3ae612639bd91614b8da0711632aa22c6
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-24 11:04:55 +0300

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,19 +2,21 @@ export {AuthService} from "./auth";
 export {AttachmentService} from "./attachments";
 export {UserService,AuthorService} from "./users/";
 export {ArticleService} from "./articles";
+export { ProductService} from "./products";
+export { PurshaseService} from "./purchases";
+export {supTicketService} from "./suptickets";
 export {UserTicketService} from "./users.tickets";
+export {PayTabService} from "./paytabs";
+export {OrgzService} from "./orgzs";
+export {PasswordService} from "./passwords";
+
+
 export {ArticleGrpcService} from "./articles.grpc";
 export {TicketGrpcService} from "./tickets.grpc";
 export {AuthGrpcService} from "./auth.grpc";
 export {AuthorGrpcService} from "./authors.grpc";
 export {UserGrpcService} from "./users.grpc";
-export {supTicketService} from "./suptickets";
 export {supTicketGrpcService} from "./suptickets.grpc";
-export {PayTabService} from "./paytabs";
-export {OrgzService} from "./orgzs";
 export {orgzGrpcService} from "./orgzs.grpc";
-export {PasswordService} from "./passwords";
-export { ProductService} from "./products";
 export { ProductGrpcService} from "./products.grpc";
 export { PurshaseGrpcService} from "./purchases.grpc";
-export { PurshaseService} from "./purchases";
