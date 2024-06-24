280d47fab5dd406225a243c02c7b7c008bf652a1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-24 19:01:18 +0300

    Update index.ts

--- src/subscribers/index.ts
+++ src/subscribers/index.ts
@@ -1,3 +1,4 @@
 export {PasswordSubscriber} from "./Password"
 export {PurshaseSubscriber} from "./Purshase";
 export {PaymentSubscriber} from "./Payment";
+export {ArticleSubscriber} from "./Article";
