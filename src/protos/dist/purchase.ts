7e162766e2de8c80b4176e621160e71e72776dd1
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-24 12:34:39 +0300

    Update purchase.ts

--- src/protos/dist/purchase.ts
+++ src/protos/dist/purchase.ts
@@ -21,6 +21,7 @@ import _m0 from "protobufjs/minimal";
 import { Error } from "./error";
 import { Any } from "./google/protobuf/any";
 import { Timestamp } from "./google/protobuf/timestamp";
+import { dateToReadable} from "../../grpc/util";
 
 export const protobufPackage = "";
 
@@ -258,10 +259,10 @@ export const Purshase = {
       obj.paymentId = Math.round(message.paymentId);
     }
     if (message.createdAt !== undefined) {
-      obj.createdAt = message.createdAt.toISOString();
+      obj.createdAt = dateToReadable( message.createdAt.toISOString());
     }
     if (message.updatedAt !== undefined) {
-      obj.updatedAt = message.updatedAt.toISOString();
+      obj.updatedAt =dateToReadable( message.updatedAt.toISOString());
     }
     return obj;
   },
