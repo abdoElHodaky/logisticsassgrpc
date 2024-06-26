5ad14fd56fb5367c811867bf9479bcef6b4d6b61
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-26 08:59:03 +0300

    Update articles.ts

--- src/controllers/articles.ts
+++ src/controllers/articles.ts
@@ -1,6 +1,6 @@
 import { services} from "../services/enum";
 import { Article  } from "../entity/"
-//import { AppDataSource } from "../_datasource";
+import { GrpcArticleController } from "./articles.grpc";
 import { CreateArticleDto } from "../dto/create-article.dto"
 import { Res, Post, Controller, Get, Body } from '@decorators/express';
 import { Response ,Request} from "express"
@@ -8,42 +8,20 @@ import { Response ,Request} from "express"
 
 @Controller('/articles')
 export class ArticleController {
+  private grpcC=new GrpcArticleController()
   
-  private articleS:any=services.Article
   
   constructor( ) {}
 
   @Get("")
-  async all():Promise<Article[]> {
-   /* 	#swagger.tags = ['Article']
-        #swagger.description = 'Endpoint to get articles' */
-    let articles:Article[]=await this.articleS.all()
-    return articles
+  async all():Promise<any> {
+   return await  this.grpcC.all()
   }
 
   @Post("")
   async create(@Res() res:Response ,@Body() createArticleDto:CreateArticleDto):Promise<Article|void>{
-   //  #swagger.tags = ['Article']
-    /*let {article,userid}=createArticleDto
-    article=<Article>{...article}
-    let author=await AppDataSource.manager.findOneByOrFail(Author,{id:parseInt(userid)})
-    article.author=author
-    author.articles.push(article)
-    await AppDataSource.manager.save(Article,article)*/
-    let article=await this.articleS.create(createArticleDto)
-    res.json({message:"created successfully"})
-    /*AppDataSource.manager.findOneByOrFail(Author,{id:userid}).then(d=>{
-        author=d;
-        return author
-    }).then(a=>{
-        article.author=a;
-        a.articles=[]
-        a.articles.push(article)
-        return article
-    }).then(a=>{
-        AppDataSource.manager.save(Article,a)
-        //AppDataSource.manager.save(a.user)
-        res.json({message:"created successfully"})
-    })*/
+   
+    return await this.grpcC.create(req?.auth?.id,createArticleDto)
+    
   }
 }
