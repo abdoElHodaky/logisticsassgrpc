6c97f20b6bc4914002f35a67c1a6ae7c5fcb2816
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-26 19:57:18 +0300

    Update payments.grpc.ts

--- src/services/payments.grpc.ts
+++ src/services/payments.grpc.ts
@@ -4,73 +4,66 @@ import  {_Payment} from "../protos/dist/";
 import {PaymentService} from "./";
 import { Payment} from "../entity/";
 
-export class ProductGrpcService  {
+export class PaymentGrpcService  {
   
 // @Service("Ticket")
-  static service:any=new ProductService()
+  static service:any=new PaymentService()
   //public [name: string]:UntypedHandleCall;
-  public SrvImpl: _Product.ProductServiceServer = {
+  public SrvImpl: _Payment.PaymentServiceServer = {
    
-    async all (call: ServerUnaryCall<_Product.GetAllProductsReq,_Product.GetAllProductsRes>,
-    callback: sendUnaryData<_Product.GetAllProductsRes>
+    async all (call: ServerUnaryCall<_Payment.GetAllPayReq,_Payment.GetAllPayRes>,
+    callback: sendUnaryData<_Payment.GetAllPayRes>
  ){
-     let products=await ProductGrpcService.service.all()
-     if(products instanceof Array){
-     let _products=products.map(_Product.Product.fromJSON)
+     let payments=await PaymentGrpcService.service.all(call.request.userId)
+     if(payments instanceof Array){
+     let _payments=payments.map(_Payment.Payment.fromJSON)
      //console.log(tickets)
-    _products.forEach((a:_Product.Product,inx:number)=>{
-     let {user,createdAt,updatedAt}=products[inx]
+    _payments.forEach((a:_Payment.Payment,inx:number)=>{
+     let {user,createdAt,updatedAt}=payments[inx]
      if(user!=null) {
       a.userId=user.id
       //console.log(a.createdAt instanceof Date)
      }
      })
        
-       let res:_Product.GetAllProductsRes={products:_products,error:{
-       Message:"",type:"",name:""}}
+       let res:_Payment.GetAllPayRes={
+         userId:call.request.userId,
+         products:_payments,
+       // error:{ Message:"",type:"",name:""}
+        }
         callback(null,res)
      }
      else{
        callback(null,{
+           userId:call.request.userId,
            products:[],
-           error:{ Message:"No Records Matched",type:"NotFoundError",name:"" }
+          // error:{ Message:"No Records Matched",type:"NotFoundError",name:"" }
          })
      }
      
       },
     async create (
-    call: ServerUnaryCall<_Product.CreateProductReq,_Product.CreateProductRes>,
-    callback: sendUnaryData<_Product.CreateProductRes>
+    call: ServerUnaryCall<_Payment.CreatePayReq,_Payment.CreatePayRes>,
+    callback: sendUnaryData<_Payment.CreatePayRes>
   ){
      //  let {userId,product}=call.request
        
       // const supticket=_Product.Product.toJSON((product!=undefined)?product:_Product.createBaseProduct())
-       let _product=await ProductGrpcService.service.create(_Product.CreateProductReq.toJSON(call.request))
-       if(_product instanceof Product){
-        const product=_Product.Product.fromJSON(_product)
+       let _product=await PaymentGrpcService.service.create(_Payment.CreatePayReq.toJSON(call.request))
+       if(_product instanceof Payment){
+        const product=_Payment.Payment.fromJSON(_product)
          product.userId=call.request?.userId
          callback(null,{
-           product:product
+           payment:product
          })
        }
       else{
         callback(null,{
-           product:_Product.createBaseProduct()
+           payment:_Payment.createBasePayment()
          })
       } 
-     },
-    async subscribe(
-      call: ServerUnaryCall<_Product.SubscribeProductReq,_Product.SubscribeProductRes>,
-      callback: sendUnaryData<_Product.SubscribeProductRes>)
-    {
-      
-    },
-    async allSubscriptions(
-      call: ServerUnaryCall<_Product.GetAllProductsSubscriptionsReq,_Product.GetAllProductsSubscriptionsRes>,
-      callback: sendUnaryData<_Product.GetAllProductsSubscriptionsRes>)
-    {
-      
-    }
+     }
+    
   }
 
   
