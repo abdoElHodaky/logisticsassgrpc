98cad6458656758d574ecdac1eb50659d1cecb45
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-27 19:09:37 +0300

    Update Subscription.ts

--- src/subscribers/Subscription.ts
+++ src/subscribers/Subscription.ts
@@ -1,1 +1,31 @@
+import {EventSubscriber ,EntitySubscriberInterface, UpdateEvent,InsertEvent } from "typeorm"
+import { Subscription,User } from "../entity/";
+@EventSubscriber()
+export class SubscriptionSubscriber implements EntitySubscriberInterface<Subscription> {
+    /**
+     * Indicates that this subscriber only listen to Post events.
+     */
+    listenTo() {
+        return Subscription
+    }
 
+    /**
+     * Called before post insertion.
+     */
+  async  beforeUpdate(event: UpdateEvent<Subscription>) {
+      /*  const {entity,manager} =event
+        if(entity?.amount){
+           entity.amount= entity.purchase.subTotal *1.14
+         } */
+    }
+    
+  async afterInsert(event:InsertEvent<Subscription>){
+     const {entity,manager} =event
+     entity.subscriber?.subscriptions.push(entity)
+    // await manager.save(User,entity.user)
+
+    }
+
+
+    
+}
