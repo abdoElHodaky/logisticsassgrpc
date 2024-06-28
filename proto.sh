8dd32a0a2e1ac59715ce0173991aa0f914011603
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-28 10:30:54 +0300

    Update proto.sh

--- proto.sh
+++ proto.sh
@@ -3,4 +3,4 @@
 #sudo ln -s "$(which node)" /usr/bin/node
 #sudo ln -s "$(which npm)" /usr/bin/npm
 
-sudo protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto  --ts_proto_out="src/protos/dist"  --ts_proto_opt=outputServices=grpc-js --ts_proto_opt=esModuleInterop=true -I=/src/protos/src/subscription.proto --experimental_allow_proto3_optional
+sudo protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto  --ts_proto_out="src/protos/dist"  --ts_proto_opt=outputServices=grpc-js --ts_proto_opt=esModuleInterop=true  -I=./src/protos/src/subscription.proto --experimental_allow_proto3_optional
