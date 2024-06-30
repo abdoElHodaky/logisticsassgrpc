5152273aca17dc8c2129e1f3a4d356be61611069
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-30 21:05:11 +0300

    Update ProductCategory.ts

--- src/entity/categories/ProductCategory.ts
+++ src/entity/categories/ProductCategory.ts
@@ -1,13 +1,28 @@
 import { Entity,Column,ManyToMany,PrimaryGeneratedColumn,JoinTable,
-        CreateDateColumn, UpdateDateColumn,ChildEntity,
-        OneToMany,ManyToOne} from "typeorm"
+        CreateDateColumn, UpdateDateColumn,ChildEntity,JoinColumn,
+        OneToMany,ManyToOne,Index} from "typeorm"
 import { Product } from "../";
 import { Category } from "./"
 @ChildEntity()
 export class ProductCategory extends Category {
    @Column({type:"varchar",default:"Product"})
    readonly forType:string="Product"
+     
+   @ManyToOne(()=>Product,product=>product.categories)
+   @JoinColumn([
+    { name: "productId", referencedColumnName: "id" },
+    //{ name: "locale_id", referencedColumnName: "locale_id" }
+    ])
+   product:Product
+   @OneToMany(()=>Product,entity=>entity.category) 
+   @JoinColumn([
+    { name: "productId", referencedColumnName: "id" },
+    //{ name: "locale_id", referencedColumnName: "locale_id" }
+    ])
+    entities:Product[]
 
-   @ManyToOne(()=>Product,product=>product.categories) product:Product
-   @OneToMany(()=>Product,entity=>entity.category) entities:Product[]
+    @Index()
+    @Column({type:"int"})
+    productId:number
+    
 }
