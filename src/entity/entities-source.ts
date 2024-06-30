4d5f95a943a10d2ff672c6960e567ffd2bf0403b
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-30 19:11:51 +0300

    Update entities-source.ts

--- src/entity/entities-source.ts
+++ src/entity/entities-source.ts
@@ -1,16 +1,14 @@
 import {
-  Article,Attachment,Product,ProductAttachment,ProductSpec,
-  Author,User,Affiliate,Purshase,Supplier,Owner,Orgz,Branch
-  ,Subscriber,PurshaseItem,Payment,supTicket,Verification,
-  OrgzAttachment, Subscription,AffiliateProduct,VerifyCode,Password,
- ProductCategory } from "./";
+  Article,Attachment,Product,ProductCategory,ProductAttachment,
+  ProductSpec,Author,User,Affiliate,Purshase,Supplier,Owner,Orgz,
+  Branch,Subscriber,PurshaseItem,Payment,supTicket,Verification,
+  OrgzAttachment, Subscription,AffiliateProduct,VerifyCode,Password } from "./";
 
 export const entities:any=[
   
-  Article,Attachment,Product,ProductAttachment,
+  Article,Attachment,Product,ProductAttachment,ProductCategory,
   ProductSpec, Author,User,Purshase,PurshaseItem,
   Payment,supTicket,Verification,Affiliate,Supplier,
   Owner,Orgz,Branch,OrgzAttachment, Subscriber,
   Password ,Subscription,AffiliateProduct,VerifyCode,
-  ProductCategory
 ]
