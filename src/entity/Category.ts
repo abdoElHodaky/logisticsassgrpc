ef5c28d87b34442750b5f1db2dd3af56630a14b7
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-30 18:50:04 +0300

    Update Category.ts

--- src/entity/Category.ts
+++ src/entity/Category.ts
@@ -29,7 +29,7 @@ export class ProductCategory extends Category {
    @Column({type:"varchar",default:"Product"})
    readonly forType:string="Product"
 
-   @ManyToMany(()=>Product,entity=>entity.categories)
+   @ManyToMany(()=>Product,product=>product.categories)
    @JoinTable()
-   entities:Product[] 
+   products:Product[] 
 }
