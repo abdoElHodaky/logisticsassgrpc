a23ef1e45df9bda5fc736f86c8cb256fb60e146f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-30 18:06:51 +0300

    Update Product.ts

--- src/entity/products/Product.ts
+++ src/entity/products/Product.ts
@@ -29,7 +29,7 @@ export class Product {
     @ManyToMany(()=>Subscription,subscrip=>subscrip.products)
     @JoinTable()
     subscriptions?:Subscription[]
-    @ManyToMany<ProductCategory>(category=>category.entities)
+    @ManyToMany<ProductCategory>(category=>category?.entities)
     @JoinTable()
     categories?:ProductCategory[]
    
