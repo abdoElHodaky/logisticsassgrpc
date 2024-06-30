b3ce771fedd7b8a4c2a21c32371bf79316433629
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-30 19:55:19 +0300

    Update ProductCategory.ts

--- src/entity/categories/ProductCategory.ts
+++ src/entity/categories/ProductCategory.ts
@@ -1,1 +1,11 @@
+import { Entity,Column,ManyToMany,PrimaryGeneratedColumn,JoinTable,
+        CreateDateColumn, UpdateDateColumn,ChildEntity } from "typeorm"
+import { Product ,Category} from "../";
+@ChildEntity()
+export class ProductCategory extends Category {
+   @Column({type:"varchar",default:"Product"})
+   readonly forType:string="Product"
 
+   @ManyToOne(()=>Product,product=>product.categories) product:Product
+   @OneToMany(()=>Product,entity=>entity.category) entities:Product[]
+}
