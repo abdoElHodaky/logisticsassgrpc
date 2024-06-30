0b1a17ce4bce275a62b0de897b9b3bbce56eb5a6
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-06-30 09:13:50 +0300

    Create Category.ts

---
+++ src/entity/Category.ts
@@ -0,0 +1,7 @@
+import { Entity,Column,ManyToOne,OneToMany,PrimaryGeneratedColumn,
+        CreateDateColumn, UpdateDateColumn,BaseEntity } from "typeorm"
+@Entity()
+export class Category<T extends BaseEntity>
+{
+  
+}
