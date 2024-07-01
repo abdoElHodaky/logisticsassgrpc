831cbf3a05d343cf17acb6f3259e334c3b718994
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-01 11:53:21 +0300

    Update categories.ts

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -1,1 +1,22 @@
+import { services } from "../services/enum";
+import { Category } from "../entity/"
+import { Res, Post, Controller, Get, Body } from '@decorators/express';
+import { Response } from "express"
+import { Request } from "express-jwt";
 
+
+@Controller('/categories')
+export class CategoryController {
+  
+  private service:any=services.Category
+  
+  constructor( ) {}
+
+  @Get("")
+  async all(@Res() res:Response):Promise<Category[]|void> {
+   /* 	#swagger.tags = ['Article']
+        #swagger.description = 'Endpoint to get articles' */
+    let attachments=await this.service.all()
+    res.json(attachments)
+   }
+  }
