42d03dfdab254f4e73bc23e746326bf5999fff29
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-01 15:28:31 +0300

    Update tsrun.sh

--- tsrun.sh
+++ tsrun.sh
@@ -1,7 +1,7 @@
 #!/user/bin/bash
 
-npx tsc && node swagautogen.js
-node dist/app.js
+npx tsc && rm swagger.json
+node swagautogen.js dist/app.js
 
 #node ./dist/grpc-reflect.js
 #npm-run-all --parallel start
