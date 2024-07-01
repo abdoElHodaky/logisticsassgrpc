340f30b1037df4058979d257b3a102b348974f8a
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-01 13:54:47 +0300

    Update categoryroute-grpc.ts

--- src/routes/categoryroute-grpc.ts
+++ src/routes/categoryroute-grpc.ts
@@ -1,14 +1,15 @@
 import { Router} from "express";
+
+
 export const categorygrpc=Router()
 
-categorygrpc.route("/categories").
-get((req,res)=>{
+categorygrpc.get("/categories",(req,res)=>{
     /* 	#swagger.tags = ['Category']
         #swagger.description = 'Endpoint to get categories via grpc'
         
         */
-}).
-post(({body},res)=>{
+})
+categorygrpc.post(({body},res)=>{
   /* 	#swagger.tags = ['Category']
         #swagger.description = 'Endpoint to add new Category via grpc' 
         #swagger.requestBody = {
