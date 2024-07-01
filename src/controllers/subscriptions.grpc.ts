815696ecbb78997d31c14e7733329c8a6ce0ce95
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-01 19:56:32 +0300

    Update subscriptions.grpc.ts

--- src/controllers/subscriptions.grpc.ts
+++ src/controllers/subscriptions.grpc.ts
@@ -1,1 +1,22 @@
+import { services } from "../services/enum";
+import { Subscription } from "../entity/"
+import { Res, Post, Controller, Get, Body } from '@decorators/express';
+import { Response } from "express"
+import { Request } from "express-jwt";
+import { Error } from "common-errors";
 
+@Controller('/supscriptions')
+export class SubscriptionController {
+  
+  private service:any=services.Subscription
+  
+  constructor( ) {}
+
+  @Get("")
+  async all(@Res() res:Response):Promise<Subscription[]|void> {
+   /* 	#swagger.tags = ['Article']
+        #swagger.description = 'Endpoint to get articles' */
+    let attachments=await this.service.all()
+    res.json((attachments instanceof Error)? attachments?.message: attachments)
+   }
+  }
