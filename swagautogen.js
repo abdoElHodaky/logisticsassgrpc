75591ef3338219aa00998f4e088226c9eff37ef7
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-01 15:20:53 +0300

    Update swagautogen.js

--- swagautogen.js
+++ swagautogen.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')({openapi:"3.0.0"});
 
-const outputFile = './src/swagger.json';
+const outputFile = './swagger.json';
 const endpointsFiles = ['./dist/controllers/*.js','./dist/routes/*-grpc.js','./dist/routes/payroute.js'];
 const config = {
     info: {
