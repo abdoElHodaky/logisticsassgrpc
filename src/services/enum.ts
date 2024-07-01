f1a766a69fab71d2c63cf6bf36108c3e0c96ffa5
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-01 12:02:11 +0300

    Update enum.ts

--- src/services/enum.ts
+++ src/services/enum.ts
@@ -1,4 +1,4 @@
-import {AuthService} from "./auth";
+/*import {AuthService} from "./auth";
 import {AttachmentService} from "./attachments";
 import {UserService,AuthorService} from "./users/";
 import {ArticleService} from "./articles";
@@ -17,6 +17,17 @@ import {orgzGrpcService} from "./orgzs.grpc";
 import {ProductGrpcService} from "./products.grpc";
 import {PurshaseGrpcService} from "./purchases.grpc";
 import {PurshaseService} from "./purchases";
+import {CategoryService} from "./categories";*/
+import {
+  AuthService,AttachmentService,UserService,AuthorService,
+  ArticleService,UserTicketService,UserTicketService,
+  supTicketService,PaymentService,OrgzService,PurshaseService,
+  CategoryService,ProductService,ArticleGrpcService,TicketGrpcService,
+  supTicketGrpcService,AuthorGrpcService,UserGrpcService,
+  AuthGrpcService,orgzGrpcService,ProductGrpcService,
+  PurshaseGrpcService
+
+} from "./";
 
 export let services:any={
   Auth:new AuthService(),
@@ -30,6 +41,13 @@ export let services:any={
   Product:new ProductService(),
   Orgz:new OrgzService(),
   Purshase:new PurshaseService(),
+  Category:new CategoryService(),
+  
+}
+
+
+export let grpc_services:any={
+  
   Grpc_Ticket:new TicketGrpcService(),
   Grpc_Article:new ArticleGrpcService(),
   Grpc_Auth:new AuthGrpcService(),
@@ -39,23 +57,5 @@ export let services:any={
   Grpc_Orgz:new orgzGrpcService(),
   Grpc_Product:new ProductGrpcService(),
   Grpc_Purshase:new PurshaseGrpcService()
-}
-
-
-export let _services:object={
-  "Auth":new AuthService(),
-  "Author":new AuthorService(),
-  "User":new UserService(),
-  "Article":new ArticleService(),
-  "Attachment":new AttachmentService(),
-  "Ticket":new UserTicketService(),
-  "supTicket":new supTicketService(),
-  "Payment":new PaymentService(),
-  "Grpc_Ticket":new TicketGrpcService(),
- " Grpc_Article":new ArticleGrpcService(),
-  "Grpc_Auth":new AuthGrpcService(),
-  "Grpc_Author":new AuthorGrpcService(),
-  "Grpc_User":new UserGrpcService(),
-  "Grpc_supTicket":new supTicketGrpcService()
   
 }
