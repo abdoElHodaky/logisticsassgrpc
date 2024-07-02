3c3b035d29770aab03b6fdc5f7ac626b53772d7f
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-02 18:57:56 +0300

    Update index.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,7 @@
 export {AuthenticateMiddleware} from "./authenticate";
 export {AuthorMiddleware} from "./author";
 export {OwnerMiddleware} from "./owner";
-export {SubscriperMiddleware} from "./subscriber";
+export {SubscriberMiddleware} from "./subscriber";
 export {UserEqulityMiddleware} from "./userequality";
 export { ValidatedLogin,
         ValidatedCreatedArticle,
