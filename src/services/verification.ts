80b24d04c0e8603fe34b43250fa91b7bb2fa2d07
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-04 12:22:33 +0300

    Update verification.ts

--- src/services/verification.ts
+++ src/services/verification.ts
@@ -1,1 +1,45 @@
 
+import {_Data} from "./datasource";
+//import { CreateArticleDto } from "../dto/create-article.dto"
+
+import { Verification,VerfyCode,VerfyType ,User } from "../entity/";
+import { Error , NotFoundError } from "common-errors";
+import { isNumeric } from "../helpers";
+export class VerificationService extends _Data {
+
+  constructor(){super()}
+  
+  async all(userId:string):Promise<Verification[]|Error>
+  {
+    if(isNumeric(userId)==true){
+     let id=Number(userId)
+    try{
+    /* let user=await this.datasource.manager.findOneOrFail(User,{where:{
+            id:id
+           },
+           relations:{
+            tickets:true
+           }
+            })*/
+    let tickets=await this.em.find(Verification,{
+      where:{user:{id:id}},cache:true
+    })
+    if(tickets.length!=0) return tickets 
+    else throw new NotFoundError("Verification")
+    }
+    catch (err:any){
+      return err
+    }
+
+    }
+    else return new TypeError("userId should be number")
+  }
+  
+  async create(userId:number):Promise<Verification|void>{
+    
+    
+    
+    
+  }
+  
+}
