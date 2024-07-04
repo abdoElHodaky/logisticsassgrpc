24bcc0cb09fea0c7fc0062c1418d38b3f4f075e9
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-04 12:39:22 +0300

    Update enum.ts

--- src/services/enum.ts
+++ src/services/enum.ts
@@ -1,33 +1,17 @@
-/*import {AuthService} from "./auth";
-import {AttachmentService} from "./attachments";
-import {UserService,AuthorService} from "./users/";
-import {ArticleService} from "./articles";
-import {UserTicketService} from "./users.tickets";
-import {ArticleGrpcService} from "./articles.grpc";
-import {TicketGrpcService} from "./tickets.grpc";
-import {AuthGrpcService} from "./auth.grpc";
-import {AuthorGrpcService} from "./authors.grpc";
-import {UserGrpcService} from "./users.grpc";
-import {supTicketGrpcService } from "./suptickets.grpc";
-import {supTicketService} from "./suptickets";
-import {PaymentService} from "./payments";
-import {ProductService} from "./products";
-import {OrgzService} from "./orgzs";
-import {orgzGrpcService} from "./orgzs.grpc";
-import {ProductGrpcService} from "./products.grpc";
-import {PurshaseGrpcService} from "./purchases.grpc";
-import {PurshaseService} from "./purchases";
-import {CategoryService} from "./categories";*/
 import {
   AuthService,AttachmentService,UserService,AuthorService,
   ArticleService,UserTicketService,supTicketService,
   PaymentService,OrgzService,PurshaseService,CategoryService,
-  ProductService,ArticleGrpcService,TicketGrpcService,
-  supTicketGrpcService,AuthorGrpcService,UserGrpcService,
-  AuthGrpcService,orgzGrpcService,ProductGrpcService,
-  PurshaseGrpcService,SubscriptionService
+  ProductService,SubscriptionService,VerificationService
 
 } from "./";
+import {
+  ArticleGrpcService,TicketGrpcService,
+  supTicketGrpcService,AuthorGrpcService,UserGrpcService,
+  AuthGrpcService,orgzGrpcService,ProductGrpcService,
+  PurshaseGrpcService,
+} from "./index.grpc";
+
 
 export let services:any={
   Auth:new AuthService(),
@@ -42,7 +26,8 @@ export let services:any={
   Orgz:new OrgzService(),
   Purshase:new PurshaseService(),
   Category:new CategoryService(),
-  Subscription: new SubscriptionService()
+  Subscription: new SubscriptionService(),
+  Verification: new VerificationService()
 }
 
 
