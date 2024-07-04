ef327212896363b8d1fedc290f4707758f983538
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-04 12:36:09 +0300

    Create index.grpc.ts

---
+++ src/services/index.grpc.ts
@@ -0,0 +1,10 @@
+
+export {ArticleGrpcService} from "./articles.grpc";
+export {TicketGrpcService} from "./tickets.grpc";
+export {AuthGrpcService} from "./auth.grpc";
+export {AuthorGrpcService} from "./authors.grpc";
+export {UserGrpcService} from "./users.grpc";
+export {supTicketGrpcService} from "./suptickets.grpc";
+export {orgzGrpcService} from "./orgzs.grpc";
+export { ProductGrpcService} from "./products.grpc";
+export { PurshaseGrpcService} from "./purchases.grpc";
