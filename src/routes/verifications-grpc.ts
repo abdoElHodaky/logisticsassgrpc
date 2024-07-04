4fb703e175ec297dccb8cacdd264cc7899eddd64
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-04 22:59:21 +0300

    Update verifications-grpc.ts

--- src/routes/verifications-grpc.ts
+++ src/routes/verifications-grpc.ts
@@ -1,1 +1,41 @@
 
+import { Router } from "express";
+//import { Author } from "../entity/Author";
+//import { Article } from "../entity/Article"
+//import { AppDataSource } from "../_datasource";
+
+export const verificationsgrpc=Router()
+
+verificationsgrpc.get("/verifications",(req,res)=>{
+    /* 	#swagger.tags = ['Verification']
+        #swagger.description = 'Endpoint to get verifications of specific user via grpc'
+        
+        */
+
+})
+
+    
+
+verificationsgrpc.post("/verifications",({body},res)=>{
+    /* 	#swagger.tags = ['Verification']
+        #swagger.description = 'Endpoint to add new verification via grpc' 
+        #swagger.requestBody = {
+            required: true,
+            content: {
+                "application/json": {
+                    schema: { $ref: '#/components/schemas/AddVerification' }  }
+                }
+            }
+        } 
+       #swagger.security = [{
+            "JWTAuth": []
+       }]
+    */
+    
+   
+})
+
+/*articlesroute.post("/verifications/validate",(req,res)=>{
+    
+})
+*/
