e55b4d6ffe481db04079507aff758e4c08c0d611
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-07-05 01:19:35 +0300

    Update verifications.ts

--- src/controllers/verifications.ts
+++ src/controllers/verifications.ts
@@ -1,1 +1,35 @@
 
+import { services } from "../services/enum";
+import { Verification ,User } from "../entity/"
+//import { AppDataSource } from "../_datasource";
+//import { CreateArticleDto } from "../dto/create-article.dto"
+import { Res, Post, Controller, Get, Body , Params ,Delete } from '@decorators/express';
+import { Response ,Request} from "express"
+import { isNumeric,nationalIdvalid } from "../helpers";
+import { Error , NotFoundError } from "common-errors";
+
+@Controller('/verifications')
+export class VerificationController {
+  
+  private  veerifyS:any=services.Verification
+  constructor(){}
+  
+  @Get("")
+  async all():Promise<User[]>{
+    //this.userS.datasource=AppDataSource
+    /* 	#swagger.tags = ['User']
+        #swagger.description = 'Endpoint to get users' */
+
+    //let resd:User[]=await AppDataSource.getRepository(User).find()
+    let users= await this.verifyS.all()
+    return users
+  }
+
+  @Post("")
+  async create(@Req() req:Request,@Res() res: Response ):Promise<any|void> 
+  {
+    return req.body
+  }
+
+  
+}
