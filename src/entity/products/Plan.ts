4cc4392f63ee909a43ad93c3d2cb1feb4513e0e2
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2024-08-16 10:46:58 +0300

    Update Plan.ts

--- src/entity/products/Plan.ts
+++ src/entity/products/Plan.ts
@@ -1,6 +1,13 @@
-import { Column,Entity} from "typeorm";
-import { Product } from "./";
+import { Entity ,Column,PrimaryGeneratedColumn,OneToOne,JoinColumn,ManyToMany,JoinTable,
+         CreateDateColumn, UpdateDateColumn,Index  } from "typeorm"
+
+import { Product,Activated ,User} from "../";
+
 @Entity()
 export class Plan extends Product {
   
+  @PrimaryGeneratedColumn("increment")
+  id:number
+  
+  @OneToOne(()=>Activated,activated=>activated.activatedPlan) client:User
 }
